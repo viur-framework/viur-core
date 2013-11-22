2a152cda8f0cc39ace40ee5fcd8fdfb7605dcd88
Tobias Steinrücken <ts@mausbrand.de>
2013-11-22 15:45:16 +0100

    Removed obsolete HRK (Human Readable Key) logic

--- modules/page.py
+++ modules/page.py
@@ -12,20 +12,8 @@ class pageSkel( HierarchySkel ):
 	searchindex = "page"
 	name = stringBone( descr="Name", indexed=True, searchable=True, required=True )
 	descr = textBone( descr="Content", required=True, searchable=True, extensions=[YouTube] )
-	hrk = baseBone( descr="Human readable key", visible=False, required=False, indexed=True, readOnly=True )
-	
-	def postProcessSerializedData( self, id,  dbfields ): #Build our human readable key
-		obj = db.Get( db.Key( id ) )
-		nr = db.Key( id ).id_or_name()
-		try:
-			name = unicode( self.name.value )
-		except:
-			name = ""
-		hrk = "!%s-%s" % ( str( "".join( [ x for x in name.lower() if x in "0123456789 abcdefghijklmnopqrstuvwxyz"] ) ).replace(" ", "_"), nr )
-		obj["hrk"] = hrk
-		db.Put( obj )
 
-class Page( Hierarchy ): 
+class Page( Hierarchy ):
 	adminInfo = {	"name": "Sites", #Name of this modul, as shown in ViUR Admin (will be translated at runtime)
 				"handler": "hierarchy",  #Which handler to invoke
 				"icon": "icons/modules/hierarchy.svg", #Icon for this modul
@@ -42,30 +30,6 @@ class Page( Hierarchy ):
 	addSkel = pageSkel
 	editSkel = pageSkel
 	viewTemplate = "page_view"
-
-	def view( self, id=None, *args, **kwargs ):
-		if unicode(id).startswith("!"): #This is a human readable key
-			repo = str(self.getAvailableRootNodes()[0]["key"])
-			query = db.Query(  self.viewSkel().kindName )
-			query.filter( "parentrepo =", repo )
-			query.filter( "hrk =", id )
-			entry = query.get()
-			if entry:
-				return( super( Page, self ).view( str( entry.key() ) ) )
-		return( super( Page, self ).view( id, *args, **kwargs  )) 
-	view.exposed = True
-
-	def pathToKey( self, key=None ): #FIXME: Obsolete
-		if unicode(key).startswith("!"): #This is a human readable key
-			repo = str(self.getAvailableRootNodes()[0]["key"])
-			query = db.Query(  self.viewSkel().kindName )
-			query.filter( "parentrepo =", repo )
-			query.filter( "hrk =", key )
-			entry = query.get()
-			if entry:
-				return( super( Page, self ).pathToKey( str( entry.key() ) ) )
-		return( super( Page, self ).pathToKey( key ))
-	pathToKey.internalExposed=True
 
 	def getAvailableRootNodes( self, *args, **kwargs ):
 		repo = self.ensureOwnModulRootNode()
