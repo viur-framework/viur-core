ad1e96b1fb4fe0dbe1d478e18605080f2ab150b8
Stefan Kögl <sk@mausbrand.de>
2014-10-08 17:00:02 +0200

    new drive version

--- modules/drive_video.py
+++ modules/drive_video.py
@@ -1,8 +1,9 @@
 __author__ = 'stefan'
 
-import logging, pprint
+import logging
+import pprint
 from server.skeleton import Skeleton
-from server.bones import stringBone
+from server.bones import stringBone, fileBone
 from server.applications.list import List
 from server.tasks import StartupTask
 
@@ -17,6 +18,7 @@ class DriveVideoSkel(Skeleton):
 	title = stringBone(descr="Video Title", required=True, indexed=True, searchable=True)
 	caption = stringBone(descr="Video Caption", required=False, indexed=True, searchable=True)
 	preview_image_url = stringBone(descr="Thumbnail Bild URL von Google", required=False, indexed=True, searchable=True)
+	thumbnail_image = fileBone(descr=u"Thumbnail Bild", required=False, multiple=False, params={"frontend_list_visible": True})
 
 
 class DriveVideoList(List):
@@ -37,46 +39,45 @@ class DriveVideoList(List):
 # @StartupTask
 def sync_with_drive():
 	"""
-		Syncs this appengine with drive content
+			Syncs this appengine with drive content
 	"""
 
-	# try:
-	by_id, by_file_id = get_stored_videos()
-	print "by_id", repr(by_id.keys())
-	print "by_file_id", repr(by_file_id.keys())
-	for file_id, video in by_file_id.items():
-		print file_id, video["caption"].value
+	try:
+		by_id, by_file_id = get_stored_videos()
+		print "by_id", repr(by_id.keys())
+		print "by_file_id", repr(by_file_id.keys())
+		for file_id, video in by_file_id.items():
+			print file_id, video["caption"].value
 
-	credentials = get_credentials()
-	drive_service = build_service(credentials)
-	folder_title = "videos"
-	filelist = retrieve_all_files(drive_service)
-	video_folder = findVideoFolder(filelist, folder_title)
-	if not video_folder:
-		video_folder = createFolder(drive_service, folder_title)
+		credentials = get_credentials()
+		drive_service = build_service(credentials)
+		folder_title = "videos"
+		filelist = retrieve_all_files(drive_service)
+		video_folder = findVideoFolder(filelist, folder_title)
+		if not video_folder:
+			video_folder = createFolder(drive_service, folder_title)
 
-	if not video_folder:
-		logging.critical("did not have an video folder in google drive. check the credentials...")
+		if not video_folder:
+			logging.critical("did not have an video folder in google drive. check the credentials...")
 
-	remote_videos = retrieve_all_files(drive_service, video_folder["id"])
+		remote_videos = retrieve_all_files(drive_service, video_folder["id"])
 
-	remote_file_ids = dict()
+		remote_file_ids = dict()
 
-	for remote_video in remote_videos:
-		print
-		pprint.pprint(remote_video)
-		print
-		if (u'explicitlyTrashed' not in remote_video or not remote_video[u'explicitlyTrashed']) and remote_video[u'mimeType'] != u'application/vnd.google-apps.folder' and remote_video["id"] not in by_file_id:
-			local_video = add_local_video(remote_video)
-			# by_id[local_video["id"]] = local_video
-			# by_file_id[local_video["file_id"]] = local_video
-			remote_file_ids[str(remote_video["id"])] = remote_video
+		for remote_video in remote_videos:
+			print
+			pprint.pprint(remote_video)
+			print
+			if (u'explicitlyTrashed' not in remote_video or not remote_video[u'explicitlyTrashed']) and remote_video[u'mimeType'] != u'application/vnd.google-apps.folder':
+				remote_file_ids[str(remote_video["id"])] = remote_video
+				if remote_video["id"] not in by_file_id:
+					local_video = add_local_video(remote_video)
 
-	# print("remote_file_ids", repr(remote_file_ids.keys()))
-	# for file_id, local_file in by_file_id.items():
-	# 	if file_id not in remote_file_ids:
-	# 		local_file["file_id"].value = "deleted"
-	# 		local_file.toDB()
+		print("remote_file_ids", repr(remote_file_ids.keys()))
+		for file_id, local_file in by_file_id.items():
+			if file_id not in remote_file_ids:
+				local_file["file_id"].value = None
+				local_file.delete()
 
-	# except Exception, err:
-	# 	logging.debug(err)
+	except Exception, err:
+		logging.exception(err)
