93bc87bac3a9bf7339edc9b35e38f9e4873aad09
Tobias Steinruecken <ts@mausbrand.de>
2015-05-01 19:33:58 +0200

    Initial Patch for SERVER-36: Remove support for "##" jinja2-line prefix

--- render/jinja2/default.py
+++ render/jinja2/default.py
@@ -141,12 +141,33 @@ class Render( object ):
 						template+".html" ]
 		for fn in fnames: #Check the templatefolder of the application
 			if os.path.isfile( os.path.join( os.getcwd(), htmlpath, fn ) ):
+				self.checkForOldLinePrefix( os.path.join( os.getcwd(), htmlpath, fn ) )
 				return( fn )
 		for fn in fnames: #Check the fallback
 			if os.path.isfile( os.path.join( os.getcwd(), "server", "template", fn ) ):
+				self.checkForOldLinePrefix( os.path.join( os.getcwd(), "server", "template", fn ) )
 				return( fn )
 		raise errors.NotFound( "Template %s not found." % template )
-	
+
+	def checkForOldLinePrefix(self, fn):
+		"""
+			This method checks the given template for lines starting with "##" - the old, now unsupported
+			Line-Prefix. Bail out if such prefixes are used. This is a temporary safety measure; will be
+			removed after 01.05.2017.
+		:param fn: The filename to check
+		:return:
+		"""
+		if not "_safeTemplatesCache" in dir( self ):
+			self._safeTemplatesCache = [] #Scan templates at most once per instance
+		if fn in self._safeTemplatesCache:
+			return #This template has already been checked and looks okay
+		tplData = open( fn, "r" ).read()
+		for l in tplData.splitlines():
+			if l.strip(" \t").startswith("##"):
+				raise SyntaxError("Template %s contains unsupported Line-Markers (##)" % fn )
+		self._safeTemplatesCache.append( fn )
+		return
+
 	def getLoaders(self):
 		"""
 			Return the List of Jinja2-Loaders which should be used.
@@ -172,7 +193,7 @@ class Render( object ):
 			htmlpath = "html/"
 		if not "env" in dir( self ):
 			loaders = self.getLoaders()
-			self.env = Environment( loader=loaders, extensions=['jinja2.ext.do', 'jinja2.ext.loopcontrols'], line_statement_prefix="##" )
+			self.env = Environment( loader=loaders, extensions=['jinja2.ext.do', 'jinja2.ext.loopcontrols'] )
 			self.env.globals["requestParams"] = self.getRequestParams
 			self.env.globals["getSession"] = self.getSession
 			self.env.globals["setSession"] = self.setSession
