cb93ac0a5c45e8e98a5a0cdb29aae2fcdfa50cde
Stefan Kögl <stefan.koegl@rub.de>
2016-01-31 15:02:13 +0100

    added prelimary fix to weak file cleanup deferred task

--- modules/file.py
+++ modules/file.py
@@ -424,6 +424,10 @@ def doDeleteWeakReferences( timeStamp, cursor ):
 	gotAtLeastOne = False
 	query = skelCls().all().filter("weak =", True).filter("creationdate <", datetime.strptime(timeStamp,"%d.%m.%Y %H:%M:%S") ).cursor( cursor )
 	for skel in query.fetch(99):
+		anyRel = any(db.Query("viur-relations").filter("dest.id =", skel["id"].value).run(1, keysOnly=True))
+		if anyRel:
+			logging.debug("doDeleteWeakReferences: found relations with that file - don't delete!")
+			continue
 		gotAtLeastOne = True
 		skel.delete()
 	newCursor = query.getCursor()
