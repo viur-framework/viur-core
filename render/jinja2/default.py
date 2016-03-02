8271d090f8db3a50a64c64ca2dd5b76904dab940
Andreas <ak@mausbrand.de>
2016-03-02 12:45:07 +0100

    subsfolder for Jinja2 Html Folder

--- render/jinja2/default.py
+++ render/jinja2/default.py
@@ -174,6 +174,10 @@ class Render( object ):
 						template+stylePostfix+".html", 
 						os.path.join(  lang, template+".html"), 
 						template+".html" ]
+		for fn in fnames: #check subfolders
+			prefix = template.split("_")[0]
+			if os.path.isfile(os.path.join(os.getcwd(), "html", prefix, fn)):
+				return ( "%s/%s" % (prefix, fn ) )
 		for fn in fnames: #Check the templatefolder of the application
 			if os.path.isfile( os.path.join( os.getcwd(), htmlpath, fn ) ):
 				self.checkForOldLinePrefix( os.path.join( os.getcwd(), htmlpath, fn ) )
