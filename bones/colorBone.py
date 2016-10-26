105a1db8f628010772c4e8b2be6a95105ba67845
Tobias Steinruecken <ts@mausbrand.de>
2016-10-26 17:38:14 +0200

    Added isInvalid() check to colorBone

--- bones/colorBone.py
+++ bones/colorBone.py
@@ -30,33 +30,34 @@ class colorBone( baseBone ):
 		else:
 			value = None
 		if self.mode!="rgb" and self.mode!="rgba":
-			return( "Invalid mode selected" )
-		if value==None:
-			return (None)
-		value=value.lower()
-		if value.count("#")>1:
-			return( "Invalid value entered" )
-		for char in value:
-			if not char in "#0123456789abcdef":
-				return( "Invalid value entered" )
-		if self.mode=="rgb":
-			if len(value)==3:
-				value="#"+value
-			if len(value)==4:
-				value=value[0:2]+value[1]+2*value[2]+2*value[3]
-			if len(value)==6 or len(value)==7:
-				if len (value)==6:
-					value="#"+value
-				valuesCache[name]=value
-				return (None)
+			return "Invalid mode selected"
+		if value is None:
+			if self.required:
+				return "No value selected"
 			else:
-				return( "Invalid value entered" )
-		if self.mode=="rgba":
-			if len(value)==8 or len(value)==9:
-				if len (value)==8:
-					value="#"+value
-				valuesCache[name]=value
-				return (None)
-			else:
-				return( "Invalid value entered" )
-		return (None)
+				value=value.lower()
+				if value.count("#")>1:
+					return "Invalid value entered"
+				for char in value:
+					if not char in "#0123456789abcdef":
+						return "Invalid value entered"
+				if self.mode=="rgb":
+					if len(value)==3:
+						value="#"+value
+					if len(value)==4:
+						value=value[0:2]+value[1]+2*value[2]+2*value[3]
+					if len(value)==6 or len(value)==7:
+						if len (value)==6:
+							value="#"+value
+					else:
+						return "Invalid value entered"
+				if self.mode=="rgba":
+					if len(value)==8 or len(value)==9:
+						if len (value)==8:
+							value="#"+value
+					else:
+						return "Invalid value entered"
+		err = self.isInvalid(value)
+		if not err:
+			valuesCache[name] = value
+		return err
