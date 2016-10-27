577552b65a79d4c10046db29adae5d0f23863198
Tobias Steinruecken <ts@mausbrand.de>
2016-10-27 18:23:43 +0200

    Replaced the regex-test with more robust checks

--- bones/emailBone.py
+++ bones/emailBone.py
@@ -1,15 +1,41 @@
 # -*- coding: utf-8 -*-
-import server
+
 from server.bones import stringBone
-import re
+import string
+from encodings import idna
+
 
 class emailBone( stringBone ):
 	type = "str.email"
 	
-	def isInvalid( self, value ):
-		regex = re.compile("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}")
-		res = regex.findall( unicode(value).lower() )
-		if len( res ) == 1 and res[0]==unicode(value).lower():
+	def isInvalid(self, value):
+		if not value:
+			return "No value entered"
+		try:
+			assert len(value) < 256
+			account, domain = value.split(u"@")
+			subDomain, tld = domain.rsplit(".", 1)
+			assert account and subDomain and tld
+			assert subDomain[0] != "."
+			assert len(account) <= 64
+		except:
+			return "Invalid email entered"
+		isValid = True
+		validChars = string.ascii_letters + string.digits + "!#$%&'*+-/=?^_`{|}~"
+		unicodeLowerBound = u"\u0080"
+		unicodeUpperBound = u"\U0010FFFF"
+		for char in account:
+			if not (char in validChars or (char >= unicodeLowerBound and char <= unicodeUpperBound)):
+				isValid = False
+		try:
+			idna.ToASCII(subDomain)
+			idna.ToASCII(tld)
+		except:
+			isValid = False
+		if " " in subDomain or " " in tld:
+			isValid = False
+		if isValid:
 			return None
 		else:
-			return server.translate("Invalid emailaddress")
+			return "Invalid email entered"
+
