79773385f4e827a7631b717c099230c87d66ecd8
Jan Max Meyer <jmm@phorward.de>
2016-12-19 12:58:19 +0100

    Resolving the login redirection to work with both Ajax requests and directly within the browser.

--- render/vi/user.py
+++ render/vi/user.py
@@ -1,15 +1,24 @@
+#-*- coding: utf-8 -*-
 from server.render.json.user import UserRender as user
-from server import utils, errors, session
-import json
-import logging
-import string
+import string, json
 
 class UserRender(user):
 
 	def loginSucceeded(self, **kwargs):
-		msg = kwargs.get("msg", "")
-		msg = "".join([x for x in msg if x in string.digits+string.ascii_letters+"-"])
+		"""
+		This loginSuccess method generates a HTML site, which redirects by meta-refresh to the Vi,
+		but can also be easily read by Ajax requests.
+		"""
+		msg = "".join([x for x in kwargs.get("msg", "OKAY") if x in string.digits+string.ascii_letters+"-"])
 
-		raise errors.Redirect("/vi/s/main.html#"+msg)
-
-		#return super(UserRender, self).loginSucceeded(**kwargs)
+		return """
+		<html>
+			<head>
+				<meta http-equiv="refresh" content="0; URL=/vi/s/main.html">
+			</head>
+			<body style="text-align: center;">
+				You will be redirected to <a href="/vi/s/main.html">/vi/s/main.html</a> in a short moment...
+				<div style="display: none;" id="msg">JSON((%s))</div>
+			</body>
+		</html>
+		""" % json.dumps(msg)
