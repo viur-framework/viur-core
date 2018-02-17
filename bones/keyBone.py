c83e6d0c61484b28da2a581530fe221645416304
Tobias Steinruecken <ts@mausbrand.de>
2018-02-17 20:11:24 +0100

    Moved normalizeKey from unserialize/serialize to refresh in keyBone

--- bones/keyBone.py
+++ bones/keyBone.py
@@ -5,18 +5,14 @@ from server.utils import normalizeKey
 class keyBone(baseBone):
 	kind = "key"
 
-	def __init__(self, descr="Key", readOnly=True, visible=False, normalize=True, **kwargs):
+	def __init__(self, descr="Key", readOnly=True, visible=False, **kwargs):
 		super(keyBone, self).__init__(descr=descr, readOnly=readOnly, visible=visible, **kwargs)
-		self.normalize = normalize
 
-	def serialize(self, valuesCache, name, entity):
-		if name != "key":
-			entity.set(name, normalizeKey(valuesCache[name]) if self.normalize else str(valuesCache[name]), self.indexed)
 
-		return entity
+	def refresh(self, valuesCache, boneName, skel):
+		"""
+			Refresh all values we might have cached from other entities.
+		"""
+		if boneName in valuesCache and valuesCache[boneName]:
+			valuesCache[boneName] = normalizeKey(valuesCache[boneName])
 
-	def unserialize(self, valuesCache, name, expando):
-		if name in expando:
-			valuesCache[name] = normalizeKey(expando[name]) if self.normalize else str(expando[name])
-
-		return True
