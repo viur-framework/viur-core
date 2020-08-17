4e4bdf3739da6d5820581ba4973bceab4833f267
Tobias Steinrücken <ts@mausbrand.de>
2020-08-17 15:46:54 +0200

    Moved projectBasePath determination to utils
    Also replaced remaining os.getCWD calls with that basePath from utils

--- skeleton.py
+++ skeleton.py
@@ -22,7 +22,6 @@ except:
 	pytz = None
 
 __undefindedC__ = object()
-projectPath = globals()["__file__"].replace("/viur/core/skeleton.py","")  # os.getCWD is broken on appengine
 
 class MetaBaseSkel(type):
 	"""
@@ -333,7 +332,7 @@ class BaseSkeleton(object, metaclass=MetaBaseSkel):
 class MetaSkel(MetaBaseSkel):
 	def __init__(cls, name, bases, dct):
 		super(MetaSkel, cls).__init__(name, bases, dct)
-		relNewFileName = inspect.getfile(cls).replace(projectPath, "")
+		relNewFileName = inspect.getfile(cls).replace(utils.projectBasePath, "")
 
 		# Check if we have an abstract skeleton
 		if cls.__name__.endswith("AbstractSkel"):
@@ -352,7 +351,7 @@ class MetaSkel(MetaBaseSkel):
 				cls.kindName = cls.__name__.lower()
 		# Try to determine which skeleton definition takes precedence
 		if cls.kindName and cls.kindName is not __undefindedC__ and cls.kindName in MetaBaseSkel._skelCache:
-			relOldFileName = inspect.getfile(MetaBaseSkel._skelCache[cls.kindName]).replace(projectPath, "")
+			relOldFileName = inspect.getfile(MetaBaseSkel._skelCache[cls.kindName]).replace(utils.projectBasePath, "")
 			idxOld = min(
 				[x for (x, y) in enumerate(conf["viur.skeleton.searchPath"]) if relOldFileName.startswith(y)] + [999])
 			idxNew = min(
