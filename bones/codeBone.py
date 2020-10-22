a4af04ec739b8cc8474086fd6e50fda722299cb7
Andreas H. Kelch <ak@mausbrand.de>
2020-10-22 07:58:07 +0200

    added codeBone

---
+++ bones/codeBone.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from viur.core.bones import baseBone
+
+class codeBone(baseBone):
+	'''
+		The codebone does not check and excaped the entered content.
+		This can lead to security problems.
+		Therefore, like the basebone, use this bone with caution.
+	'''
+	type = "code"
