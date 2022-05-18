d4cd41246e42f1d9f2edbdd5265f326f471f9191
Tobias Steinrücken <ts@mausbrand.de>
2022-05-18 17:22:03 +0200

    Fixed treeNodeBone enforcing "_rootNode" suffix on it's kind

--- core/bones/treeNodeBone.py
+++ core/bones/treeNodeBone.py
@@ -8,6 +8,4 @@ class treeNodeBone(relationalBone):
 	type = "relational.tree.node"
 
 	def __init__(self, *, kind=None, format="value['dest']['name']", **kwargs):
-		if kind and not kind.endswith("_rootNode"):
-			kind += "_rootNode"
 		super(treeNodeBone, self).__init__(kind=kind, format=format, *args, **kwargs)
