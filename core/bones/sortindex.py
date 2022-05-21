b07a01f337ec70ee501d3828a771fc035f524e1a
Jan Max Meyer <jmm@phorward.de>
2022-05-21 13:55:23 +0200

    Added type annotations

--- core/bones/sortindex.py
+++ core/bones/sortindex.py
@@ -1,23 +1,23 @@
 from viur.core.bones.numeric import NumericBone
-import time
+import time, typing
 
 
 class SortIndexBone(NumericBone):
-	type = "numeric.sortindex"
+    type = "numeric.sortindex"
 
-	def __init__(
-		self,
-	    *,
-		defaultValue=lambda: time.time(),
-		descr="SortIndex",
-		max=pow(2, 30),
-		precision=8,
-		**kwargs
-	):
-		super().__init__(
-			defaultValue=defaultValue,
-			descr=descr,
-			max=max,
-			precision=precision,
-			**kwargs
-		)
+    def __init__(
+        self,
+        *,
+        defaultValue: typing.Union[int, float] = lambda: time.time(),
+        descr: str = "SortIndex",
+        max: typing.Union[int, float] = pow(2, 30),
+        precision: int = 8,
+        **kwargs
+    ):
+        super().__init__(
+            defaultValue=defaultValue,
+            descr=descr,
+            max=max,
+            precision=precision,
+            **kwargs
+        )
