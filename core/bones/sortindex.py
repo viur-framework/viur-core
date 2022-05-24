37243f3e3d3bb1cbc42d92132a544dbd8ae09b74
Jan Max Meyer <jmm@phorward.de>
2022-05-24 16:38:15 +0200

    Update core/bones/sortindex.py
    Use *args, **kwargs instead of full-qualified names here.

--- core/bones/sortindex.py
+++ core/bones/sortindex.py
@@ -8,7 +8,7 @@ class SortIndexBone(NumericBone):
     def __init__(
         self,
         *,
-        defaultValue: typing.Union[int, float] = lambda skeletonInstance, bone: time.time(),
+        defaultValue: typing.Union[int, float] = lambda *args, **kwargs: time.time(),
         descr: str = "SortIndex",
         max: typing.Union[int, float] = pow(2, 30),
         precision: int = 8,
