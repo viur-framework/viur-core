012dce93f975b090fdda009a8f91c1c34326cd86
Jan Max Meyer <jmm@phorward.de>
2022-07-06 13:32:34 +0200

    setuptools' install_requires from requirements.txt (#475)
    * setuptools' install_requires from requiremens.txt
    
    This draft reads the `install_requires` configuration directly from the requirements.txt and removes the need for a double requirement maintainance from both requirements.txt and previously setup.cfg.
    
    This attempt is the result of the discussion in https://github.com/pypa/setuptools/discussions/3433.
    
    This branch has been published as viur-core==3.2.0-rc4 for testing purposes.
    
    * Fixing requirements.txt's stupid "\\" notation
    
    * Correcting pillow==9.1.1 hashes in requirements.txt

--- setup.py
+++ setup.py
@@ -2,5 +2,10 @@ import setuptools
 
 setuptools.setup(
     package_dir={'viur': '.'},
-    packages=[f'viur.{mod}' for mod in setuptools.find_packages('.', exclude=('tests*',))]
+    packages=[f'viur.{mod}' for mod in setuptools.find_packages('.', exclude=('tests*',))],
+    install_requires=sorted([
+        line.split(maxsplit=1)[0].strip(" \t;")
+        for line in open("requirements.txt").readlines()
+        if "==" in line and not line.strip().startswith("#")
+    ], key=lambda k: k.lower())
 )
