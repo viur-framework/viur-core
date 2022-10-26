7f65a819f554d513c319ba5fa4056770656c5805
Sven Eberth <se@mausbrand.de>
2022-10-26 14:52:59 +0200

    Make @jinjaGlobalTest callable to provide an alias

--- core/render/html/env/tests.py
+++ core/render/html/env/tests.py
@@ -4,13 +4,13 @@ from ..default import Render
 from ..utils import jinjaGlobalTest
 
 
-@jinjaGlobalTest
+@jinjaGlobalTest("dict")
 def test_dict(render: Render, value: Any) -> bool:
     """Jinja2 test: Return True if the object is a dict."""
     return isinstance(value, dict)
 
 
-@jinjaGlobalTest
+@jinjaGlobalTest("list")
 def test_list(render: Render, value: Any) -> bool:
     """Jinja2 test: Return True if the object is a list."""
     return isinstance(value, list)
--- core/render/html/utils.py
+++ core/render/html/utils.py
@@ -1,3 +1,5 @@
+from typing import Callable, Union
+
 __jinjaGlobals_ = {}
 __jinjaFilters_ = {}
 __jinjaTests_ = {}
@@ -36,14 +38,37 @@ def jinjaGlobalFilter(f):
     return f
 
 
-def jinjaGlobalTest(f):
+def jinjaGlobalTest(func_or_alias: Union[Callable, str]) -> Callable:
     """
     Decorator, marks a function as a Jinja2 test.
 
-    The method name can start with "test_" to avoid name conflicts.
+    To avoid name conflicts you can call the decorator
+    with an alias as first argument.
+    Otherwise, the test will be registered under the function name.
+
+    Example:
+        >>> from viur.core.render.html import jinjaGlobalTest
+        >>> # @jinjaGlobalTest  # available under "positive_number"
+        >>> @jinjaGlobalTest("positive")  # available under "positive"
+        >>> def positive_number(render, value):
+        >>>     return isinstance(value, int) and value > 0
     """
-    __jinjaTests_[f.__name__.lstrip("test_")] = f
-    return f
+    if callable(func_or_alias):  # is func
+        __jinjaTests_[func_or_alias.__name__] = func_or_alias
+        return func_or_alias
+
+    elif isinstance(func_or_alias, str):  # is alias
+        def wrapper(func):
+            __jinjaTests_[func_or_alias] = func
+            return func
+
+        return wrapper
+
+    else:
+        raise TypeError(
+            f"jinjaGlobalTest must be called with a function (used as decorator) "
+            f"or a string (alias). But got {type(func_or_alias)}."
+        )
 
 
 def jinjaGlobalExtension(ext):
