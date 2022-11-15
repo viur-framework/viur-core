4fab77470fb4dee326311ac1fc692b5979b08452
Arne Gudermann <ag@mausbrand.de>
2022-11-15 10:50:13 +0100

    Rename key->module_name

--- core/modules/modulesconf.py
+++ core/modules/modulesconf.py
@@ -8,19 +8,16 @@ from viur.core.prototypes import List, BasicApplication
 
 class ModulesConfSkel(Skeleton):
     kindName = "viur-modules-conf"
-    name = StringBone(descr=translate("modulename"),readOnly=True)
-    help_text=StringBone(descr=translate("module helptext"))
-    help_text_add=StringBone(descr=translate("add helptext"))
-    help_text_edit=StringBone(descr=translate("edit helptext"))
-    #seo.....
+    name = StringBone(descr=translate("modulename"), readOnly=True)
+    help_text = StringBone(descr=translate("module helptext"))
+    help_text_add = StringBone(descr=translate("add helptext"))
+    help_text_edit = StringBone(descr=translate("edit helptext"))
+    # seo.....
 
 
 class ModulesConf(List):
     kindName = "viur-modules-conf"
     accessRights = ["manage"]
-
-    def __init__(self, moduleName, modulePath, *args, **kwargs):
-        super(ModulesConf, self).__init__(moduleName, modulePath, *args, **kwargs)
 
     def canAdd(self):
         return False
@@ -28,19 +25,18 @@ class ModulesConf(List):
     def canDelete(self, skel: SkeletonInstance) -> bool:
         return False
 
+
 @StartupTask
 def read_all_modules():
-    for key in dir(conf["viur.mainApp"].vi):
-
-        app = getattr(conf["viur.mainApp"].vi, key)
+    for module_name in dir(conf["viur.mainApp"].vi):
+        app = getattr(conf["viur.mainApp"].vi, module_name)
         if isinstance(app, BasicApplication):
-            dbkey = db.Key("viur-modules-conf", f"{key}")
-            if not db.Get(dbkey):
-                skel = getattr(conf["viur.mainApp"].vi, "_modulesconf").addSkel()
-                skel["key"] = dbkey
-                skel["name"] = key
+            db_key = db.Key("viur-modules-conf", f"{module_name}")
+            if not db.Get(db_key):
+                skel = getattr(conf["viur.mainApp"], "_modulesconf").addSkel()
+                skel["key"] = db_key
+                skel["name"] = module_name
                 skel.toDB()
 
 
-ModulesConf.html = True
 ModulesConf.json = True
