24ccd2f64b73f53ecc2fc7c41bd11bfa2d8690c1
Andreas H. Kelch <ak@mausbrand.de>
2022-11-30 07:59:46 +0100

    fix: add missing import

--- core/request.py
+++ core/request.py
@@ -17,6 +17,7 @@ from viur.core.config import conf
 from viur.core.logging import client as loggingClient, requestLogger, requestLoggingRessource
 from viur.core.securityheaders import extendCsp
 from viur.core.utils import currentLanguage, currentSession
+from viur.core.tasks import _appengineServiceIPs
 
 """
     This module implements the WSGI (Web Server Gateway Interface) layer for ViUR. This is the main entry
