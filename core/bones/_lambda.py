74ae259a0329f45dd1db333d62050f5b7b855a48
Arne Gudermann <ag@mausbrand.de>
2023-01-18 10:08:24 +0100

    Add threshold and remove eval

--- core/bones/_lambda.py
+++ core/bones/_lambda.py
@@ -1,39 +1,32 @@
-from typing import Union, Callable
-from viur.core.bones.raw import RawBone
+import logging
+from datetime import datetime, timedelta
+from typing import Callable
+from viur.core.bones.base import BaseBone
+from viur.core import utils
 
 
-class LambdaBone(RawBone):
+class LambdaBone(BaseBone):
     """
-        This bone return  value from a python function or from eval
+        This bone return  value from a python function
     """
 
-    type = "raw.lambda"
+    type = "lambda"
 
     def __init__(self, indexed: bool = False, multiple: bool = False, languages: bool = None, readonly: bool = True,
-                 code: Union[str, Callable] = None,
-                 provide_skel: bool = False,
+                 evaluate: Callable = None,
+                 threshold: int = 0,
                  *args, **kwargs):
         """
                 Initializes a new LambdaBone.
-                :param code
+                :param evaluate
                     The code can be a lamda function:
-                        >>> LambdaBone(code=lambda : "Hello "+"World")
+                        >>> LambdaBone(evaluate=lambda __skel__ : "Hello "+"World")
 
                     It can be a function:
-                        >>> def test():
-                        >>>     return "Hello World"
-                        >>> LambdaBone(code=test)
-
-                    It can be a valid eval expression:
-                        >>> LambdaBone(code="'Hello'+'World'")
-                :param provide_skel With this you can pass the skeleton instance to the function.This only works when
-                    code is a function with the parameter __skel__ or with **kwargs.
-                    Example:
-                        We can access other Bones inside the test function
                         >>> def test(__skel__):
-                        >>>     return __skel__.["otherBone"]
-                        >>> otherBone=StringBone()
-                        >>> LambdaBone(code=test, provide_skel=True)
+                        >>>     return "Hello World"
+                        >>> LambdaBone(evaluate=test)
+                :param threshold The time in minutes before the value is unvalid
 
 
         """
@@ -42,23 +35,49 @@ class LambdaBone(RawBone):
         assert not indexed
         assert readonly  # Can we trust our User
         super().__init__(*args, **kwargs)
-        self.code = code
-        self.provide_skel = provide_skel
+        self.evaluate = evaluate
+        self.threshold = threshold
 
     def serialize(self, skel: 'SkeletonInstance', name: str, parentIndexed: bool) -> bool:
+        if name in skel.accessedValues:
+            skel.dbEntity[name] = skel.accessedValues[name]
+
+            # Ensure this bone is NOT indexed!
+            skel.dbEntity.exclude_from_indexes.add(name)
+
+            return True
+
         return False
 
     def unserialize(self, skel: 'viur.core.skeleton.SkeletonInstance', name: str) -> bool:
-        if callable(self.code):
-            if self.provide_skel:
-                tmp_skel = skel.clone()
-                tmp_skel[name] = None  # we must remove our bone because of recursion
-                skel.accessedValues[name] = self.code(__skel__=tmp_skel)
+        res = {}
+        if data := skel.dbEntity.get(name):
+            if data["valid_until"] > utils.utcNow():
+                res = data
             else:
-                skel.accessedValues[name] = self.code()
-            return True
-        if isinstance(self.code, str):
-            skel.accessedValues[name] = eval(self.code)
-            return True
+                if callable(self.evaluate):
+                    res["value"] = self.evaluate_function(skel, name)
+                    res["valid_until"] = utils.utcNow() + timedelta(minutes=self.threshold)
 
+            skel.accessedValues[name] = res
+
+            return True
+        else:
+            if callable(self.evaluate):
+                res["value"] = self.evaluate_function(skel, name)
+                res["valid_until"] = utils.utcNow() + timedelta(seconds=self.threshold)
+                skel.accessedValues[name] = res
+                return True
         return False
+
+    def evaluate_function(self, skel: 'viur.core.skeleton.SkeletonInstance', name: str):
+        tmp_skel = skel.clone()
+        tmp_skel[name] = None  # we must remove our bone because of recursion
+        return self.evaluate(__skel__=tmp_skel)
+
+    def refresh(self, skel: 'viur.core.skeleton.SkeletonInstance', boneName: str) -> None:
+        """
+            Refresh the output of the self.evaluate fuction
+        """
+        skel[boneName] = {"value": self.evaluate_function(skel, boneName),
+                          "valid_until": utils.utcNow() + timedelta(seconds=self.threshold)}
