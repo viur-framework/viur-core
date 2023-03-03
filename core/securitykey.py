9d2220034851d136a516b07c375396e81cf0ecf7
Jan Max Meyer <jmm@phorward.de>
2023-03-03 19:11:35 +0100

    Refactoring `securitykey` module (#656)
    This is a refactoring of the entire module without functional changes.
    
    Co-authored-by: Sven Eberth <mail@sveneberth.de>

--- core/securitykey.py
+++ core/securitykey.py
@@ -1,105 +1,122 @@
 """
-    This module provides onetime keys.
+    This module provides one-time keys.
+
     There are two types of security keys:
-    - If :meth:create is called without arguments, it returns the current session CSRF token. Repeated calls to
+
+    1. If :meth:create is called without arguments, it returns the current session CSRF token. Repeated calls to
         :meth:create will return the same CSRF token (for the same session) until that token has been redeemed.
-        This security key will be valid as long the session is active and it's not possible to store data along with
+        This security key will be valid as long the session is active, and it's not possible to store data along with
         that key. These are usually used as a CSRF token.
         This has been changed from ViUR2 - where it was possible to create a arbitrary number of security keys per
         session.
-    - If :meth:create is called with a duration (and optional keyword-parameters), it will create a security key
-        that is *not* bound to the current session, but it's possible to store custom data (the excess keyword
-        arguments passed to :meth:create). As these are not bound to the session, each call to :meth:create will yield
-        a new token. These are used if it's expected that the token may be redeemed on a different device (eg. when
-        sending an email address confirmation email)
+    2. If :meth:create is called with a duration (and optional kwargs values), it will create a security key
+        that is *not* bound to the current session, but it's possible to store custom data (provided by kwargs).
+        As these are not bound to the session, each call to :meth:create will yield a new token.
+        These are used if it's expected that the token may be redeemed on a different device (e.g. when sending an
+        email address confirmation)
 
-    ..note: There's a hidden 3rd type of security-key: The sessions static security key. This key is only revealed once
-        (during login, as the protected header Sec-X-ViUR-StaticSKey). This can be used instead of the onetime sessions
-        security key by sending it back as the same protected http header and setting the skey value to
-        "staticSessionKey". This is only intended for non-webbrowser, programmatic access
-        (ViUR Admin, import tools etc) where CSRF attacks are not applicable. Therefore that header is prefixed with
-        "Sec-" - so it cannot be read or set by javascript.
+    ..note:
+        There's also a hidden 3rd type of security-key: The sessions static security key.
+
+        This key is only revealed once (during login, as the protected header Sec-X-ViUR-StaticSKey).
+
+        This can be used instead of the one-time sessions security key by sending it back as the same protected HTTP
+        header and setting the skey value to "staticSessionKey". This is only intended for non-web-browser,
+        programmatic access (admin tools, import tools etc.) where CSRF attacks are not applicable.
+
+        Therefor that header is prefixed with "Sec-" - so it cannot be read or set using JavaScript.
 """
+from viur.core import utils, current, db, tasks
 from datetime import datetime, timedelta
-from viur.core.utils import generateRandomString
-from viur.core import current, db
-from viur.core.tasks import PeriodicTask, CallDeferred
 from typing import Union
-from viur.core.utils import utcNow
 
-securityKeyKindName = "viur-securitykeys"
+SECURITYKEY_KINDNAME = "viur-securitykey"
 
 
-def create(duration: Union[None, int] = None, **kwargs) -> str:
+def create(duration: Union[None, int] = None, **custom_data) -> str:
     """
-        Creates a new onetime Securitykey or returns the current sessions csrf-token.
-        The custom data (given as keyword arguments) that can be stored with the key if :param:duration is set must
-        be serializable by the datastore.
+        Creates a new one-time security key or returns the current sessions CSRF-token.
 
-        :param duration: Make this key valid for a fixed timeframe (and independent of the current session)
-        :returns: The new onetime key
+        The custom data (given as **custom_data) that can be stored with the key.
+        Any data provided must be serializable by the datastore.
+
+        :param duration: Make this key valid for a fixed timeframe of seconds (and independent of the current session)
+        :returns: The new one-time key, which is a randomized string.
     """
     if not duration:
-        assert not kwargs, "kwargs are not allowed when session security key is wanted"
+        if custom_data:
+            raise ValueError("Providing any custom_data is not allowed when session security key is wanted")
+
         return current.session.get().getSecurityKey()
-    key = generateRandomString()
-    duration = int(duration)
-    dbObj = db.Entity(db.Key(securityKeyKindName, key))
-    for k, v in kwargs.items():
-        dbObj[k] = v
-    dbObj["until"] = utcNow() + timedelta(seconds=duration)
-    db.Put(dbObj)
+
+    elif "until" in custom_data:
+        raise ValueError("The 'until' property is reserved and cannot be used in the custom_data")
+
+    key = utils.generateRandomString()
+
+    entity = db.Entity(db.Key(SECURITYKEY_KINDNAME, key))
+    entity |= custom_data
+
+    entity["until"] = utils.utcNow() + timedelta(seconds=int(duration))
+    db.Put(entity)
+
     return key
+
 
 def validate(key: str, useSessionKey: bool) -> Union[bool, db.Entity]:
     """
-        Validates a security key. If useSessionKey is true, the key is expected to be the sessions current security key
-        (or it's static security key). Otherwise it must be a key created with a duration (so it's not session
-        dependent)
+        Validates a security key.
 
-        :param key: The key to validate
-        :param useSessionKey: If True, we validate against the session's skey, otherwise we'll lookup an unbound key
+        If useSessionKey is True, the key is expected to be the session's current security key
+        or its static security key.
+
+        Otherwise, it must be a key created with a duration, so that it is session independent.
+
+        :param key: The key to be validated
+        :param useSessionKey: If True, validate against the session's skey, otherwise lookup an unbound key
         :returns: False if the key was not valid for whatever reasons, the data (given during createSecurityKey) as
-            dictionary or True if the dict is empty (or :param:useSessionKey was true).
+            dictionary or True if the dict is empty (or useSessionKey was True).
     """
     if useSessionKey:
         session = current.session.get()
         if key == "staticSessionKey":
-            request = current.request.get()
-            skeyHeaderValue = request.request.headers.get("Sec-X-ViUR-StaticSKey")
-            if skeyHeaderValue and session.validateStaticSecurityKey(skeyHeaderValue):
+            skey_header_value = current.request.get().request.headers.get("Sec-X-ViUR-StaticSKey")
+            if skey_header_value and session.validateStaticSecurityKey(skey_header_value):
                 return True
+
         elif session.validateSecurityKey(key):
             return True
+
         return False
-    if not key:
+
+    if not key or not (entity := db.Get(db.Key(SECURITYKEY_KINDNAME, key))):
         return False
-    dbKey = db.Key(securityKeyKindName, key)
-    dbObj = db.Get(dbKey)
-    if dbObj:
-        db.Delete(dbKey)
-        until = dbObj["until"]
-        if until < utcNow():  # This key has expired
-            return False
-        del dbObj["until"]
-        if not dbObj:
-            return True
-        return dbObj
-    return False
+
+    db.Delete(entity)
+
+    # Key has expired?
+    if entity["until"] < utils.utcNow():
+        return False
+
+    del entity["until"]
+
+    return entity or True
 
 
-@PeriodicTask(60 * 4)
-def startClearSKeys() -> None:
+@tasks.PeriodicTask(60 * 4)
+def start_clear_skeys():
     """
         Removes old (expired) skeys
     """
-    doClearSKeys((datetime.now() - timedelta(seconds=300)).strftime("%d.%m.%Y %H:%M:%S"))
+    do_clear_skeys(datetime.now() - timedelta(seconds=300))
 
 
-@CallDeferred
-def doClearSKeys(timeStamp: str) -> None:
-    query = db.Query(securityKeyKindName).filter("until <", datetime.strptime(timeStamp, "%d.%m.%Y %H:%M:%S"))
+@tasks.CallDeferred
+def do_clear_skeys(until: datetime):
+    query = db.Query(SECURITYKEY_KINDNAME).filter("until <", until)
+
     for oldKey in query.run(100):
         db.Delete(oldKey)
+
     if query.getCursor():
-        doClearSKeys(timeStamp)
+        do_clear_skeys(until)
