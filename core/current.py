9a5e4e4abef9cf3b209cb01e77a5fcc4f8fd9cb6
Arne Gudermann <ag@mausbrand.de>
2023-03-13 18:11:40 +0100

    Improvements

--- core/current.py
+++ core/current.py
@@ -6,22 +6,22 @@ session = ContextVar("Session", default=None)
 language = ContextVar("Language", default=None)
 user = ContextVar("User", default=None)
 
-
 class CurrentUser:
     def __init__(self):
-        self.loaded = False
-        self.value = None
-
+        print("in init")
     def __repr__(self):
-        if self.loaded:
-            return self.value
-        self.loaded = True
         from viur.core import conf  # noqa: E402 # import works only here because circular imports
         if user_mod := getattr(conf["viur.mainApp"], "user", None):
-            self.value = user_mod.getCurrentUser()
-            return self.value
-
+            user.set(user_mod.getCurrentUser())
+            print("user")
+            print(type(user.get()))
+            return user.get()
     def __str__(self):
         return str(self.__repr__())
+    def __new__(cls):
+        print("in new")
+        print(f'Creating a new {cls.__name__} object...')
+        obj = object.__new__(cls)
+        return obj
 
 user.set(CurrentUser())
