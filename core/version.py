7f7bc02c1467022af64f3b3d75f0a28cce816854
Jan Max Meyer <jmm@phorward.de>
2023-04-14 09:45:30 +0200

    Improved clarification for the __version__ string

--- core/version.py
+++ core/version.py
@@ -1,1 +1,6 @@
-__version__ = "3.4.0-beta3"
+# This is the place where the viur-core version number is defined;
+# For pre-releases, postfix with ".betaN" or ".rcN" where `N` is an incremented number for each pre-release.
+# This will mark it as a pre-release as well on PyPI.
+
+__version__ = "3.4.0.rc1"
+assert __version__.count(".") == 3  # semantic version number is mandatory!
