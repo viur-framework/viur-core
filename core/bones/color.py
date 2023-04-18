fa1531f8a68767dbed26114945e819943e5ad18c
Claudia Dietrich <claudia.sollinger@gmail.com>
2023-04-18 11:56:38 +0200

    DOC: docstrings for ColorBone

--- core/bones/color.py
+++ core/bones/color.py
@@ -1,9 +1,20 @@
+"""
+The class ColorBone is used to store color values. It inherits from the BaseBone class.
+"""
 from viur.core.bones.base import BaseBone, ReadFromClientError, ReadFromClientErrorSeverity
 from typing import List, Union
 import logging
 
 
 class ColorBone(BaseBone):
+    """
+    ColorBone is a custom bone class for storing color values in the ViUR framework.
+    It inherits from the BaseBone class in the viur.core.bones.base module.
+
+    :param type: A string representing the bone type, set to "color".
+    :param mode: A string specifying the color mode, either "rgb" or "rgba". Default is "rgb".
+    :param **kwargs: Additional keyword arguments passed to the BaseBone constructor.
+    """
     type = "color"
 
     def __init__(self, *, mode="rgb", **kwargs):  # mode rgb/rgba
@@ -12,6 +23,17 @@ class ColorBone(BaseBone):
         self.mode = mode
 
     def singleValueFromClient(self, value, skel: 'viur.core.skeleton.SkeletonInstance', name: str, origData):
+        """
+        Processes a single value from the client, ensuring it is a valid color value,
+        and returns a tuple containing the processed value and any errors that occurred.
+
+        :param value: The value to be processed.
+        :param skel: The skeleton instance associated with the value.
+        :param name: The name of the bone.
+        :param origData: The original data for the bone.
+
+        :return tuple: A tuple containing the processed value if valid, or the empty value if invalid, and a list of ReadFromClientError instances if there were errors, or None if no errors occurred.
+        """
         value = value.lower()
         if value.count("#") > 1:
             return self.getEmptyValue(), [
