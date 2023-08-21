9990da96ef11d7176373ebfb7dac99452ee57616
Jan Max Meyer <jmm@phorward.de>
2023-08-22 00:18:54 +0200

    Adding `secret` module

---
+++ src/viur/core/secret.py
@@ -0,0 +1,27 @@
+import logging
+from google.cloud import secretmanager
+from viur.core.config import conf
+
+"""
+This module provides utiliy functions for accessing values stored in the Google Cloud Secret Manager.
+"""
+
+# Global secret manager client instance
+__client = secretmanager.SecretManagerServiceClient()
+
+
+def get(secret: str, version: str = "latest") -> str:
+    """
+    Retrieves a secret stored in Google Cloud Secret Manager for use within the application.
+
+    Add a secret online under https://console.cloud.google.com/security/secret-manager.
+    Service accounts requires the role "Secret Manager Secret Accessor" in IAM.
+    """
+    name = f"""projects/{conf["viur.instance.project_id"]}/secrets/{secret}/versions/{version}""".replace(".", "_")
+
+    try:
+        return __client.access_secret_version(request={"name": name}).payload.data.decode()
+    except Exception as e:
+        logging.exception(e)
+
+    return None
