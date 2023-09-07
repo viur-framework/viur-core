bbbbb853fa02197e8d6cad65b8ecc22a76403418
Jan Max Meyer <jmm@phorward.de>
2023-09-07 21:25:38 +0200

    refactor: Prototype action functions (#831)
    - Consequent use of "key" in signature
    - Clean-up kwargs check
    - Clean-up "bounce" check
    - Make code more readable
    - Instanciate variables right before their use, and not on top
    ---------
    
    Co-authored-by: agudermann <47318461+ArneGudermann@users.noreply.github.com>

--- src/viur/core/prototypes/singleton.py
+++ src/viur/core/prototypes/singleton.py
@@ -1,5 +1,5 @@
 import logging
-from typing import Any, Optional
+from typing import Any
 from viur.core import db, current, errors
 from viur.core.decorators import *
 from viur.core.cache import flushCache
@@ -134,18 +134,19 @@ class Singleton(SkelModule):
         :raises: :exc:`viur.core.errors.Unauthorized`, if the current user does not have the required permissions.
         :raises: :exc:`viur.core.errors.PreconditionFailed`, if the *skey* could not be verified.
         """
-        skel = self.editSkel()
-
         if not self.canEdit():
             raise errors.Unauthorized()
 
         key = db.Key(self.editSkel().kindName, self.getKey())
-
+        skel = self.editSkel()
         if not skel.fromDB(key):  # Its not there yet; we need to set the key again
             skel["key"] = key
-        if (len(kwargs) == 0  # no data supplied
+
+        if (
+            not kwargs  # no data supplied
             or not skel.fromClient(kwargs)  # failure on reading into the bones
-            or ("bounce" in kwargs and kwargs["bounce"] == "1")):  # review before changing
+            or kwargs.get("bounce") == "1"  # review before changing
+        ):
             return self.render.edit(skel)
 
         self.onEdit(skel)
@@ -153,7 +154,7 @@ class Singleton(SkelModule):
         self.onEdited(skel)
         return self.render.editSuccess(skel)
 
-    def getContents(self) -> Optional[SkeletonInstance]:
+    def getContents(self) -> SkeletonInstance | None:
         """
         Returns the entity of this singleton application as :class:`viur.core.skeleton.Skeleton` object.
 
