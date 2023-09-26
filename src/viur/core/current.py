3edff509a9118bba5a14ca6fc54dfc3e73a1d17a
Sven Eberth <se@mausbrand.de>
2023-09-26 18:57:54 +0200

    feat: Add missing type hints for `current` module (#871)
    Using forward references and `typing.TYPE_CHECKING` to avoid circular imports.

--- src/viur/core/current.py
+++ src/viur/core/current.py
@@ -1,7 +1,13 @@
 from contextvars import ContextVar
+from typing import Optional, TYPE_CHECKING
 
-request = ContextVar("Request", default=None)
-request_data = ContextVar("Request-Data", default=None)
-session = ContextVar("Session", default=None)
-language = ContextVar("Language", default=None)
-user = ContextVar("User", default=None)
+if TYPE_CHECKING:
+    from .request import Router
+    from .session import Session
+    from .skeleton import SkeletonInstance
+
+request: ContextVar[Optional["Router"]] = ContextVar("Request", default=None)
+request_data: ContextVar[Optional[dict]] = ContextVar("Request-Data", default=None)
+session: ContextVar[Optional["Session"]] = ContextVar("Session", default=None)
+language: ContextVar[Optional[str]] = ContextVar("Language", default=None)
+user: ContextVar[Optional["SkeletonInstance"]] = ContextVar("User", default=None)
