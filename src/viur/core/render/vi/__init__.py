viur-framework/viur-core

commit ed77ae6680d5b288617668cd3222814587f1c8f1

feat: Implement the config as a class (#833)
This allows to group config settings, use docstrings, type hints, and
finally the IDE can autocomplete the names. This implementation is 100 %
backwards compatible.


## Summary of changes
- Eliminate the old config dict
- Replaced by nested objects
- Add, adjust docstrings
- Add type hints
- Rearranged config items, renamed to snake_case (PEP8 compliant)
- Add backward compatibility for old names and structure
- Add strict mode (which enforces attributes access and new names) = no
backward compatibility
  - enable via environment variable `VIUR_CORE_CONFIG_STRICT_MODE`
- Refactored all conf usages to new syntax, structure and names
- Add unit tests for config (coverage is around 93 %)

## strict mode
Strict mode enforces the attribute access and predefined config
variables. If disabled you have backward compatibility, and can use the
dict-like access `conf["viur.mainApp"]`.

Since the app_server doesn't support loading of env variables
(https://github.com/viur-framework/viur-app_server/issues/1) you have to
start your server with
```sh
$ VIUR_CORE_CONFIG_STRICT_MODE=True pipenv run viur run ...
```
to enable strict mode.
In the cloud you can add
```yaml
env_variables:
    VIUR_CORE_CONFIG_STRICT_MODE: True
```
to your `app.yaml`.


## ToDo (remaining points, will be done in a follow-up):

- [x] strict mode (not legacy)
- [x] ~instantiate the admin class instead of subclass~ (bad idea: then
we have no docstrings)
- [x] remove viur-prefix?!
- [x] Replace all `conf.get("...key...")` usages, they are not
compatible with strict mode (all configs should have an default value)
- [x] Add type annotations
- [ ] copy docs from
https://github.com/viur-framework/viur-core/blob/d2365bc9701da6fd320db48d4bac8448bc52f598/docs/doc_tutorial/advanced/05_training_config.rst
into docstrings and remove the tutorial
- [x] remove debugs
- [ ] Sort alphabetic or logical?

src/viur/core/render/vi/__init__.py
@@ -29,7 +29,7 @@ def getStructure(module):
     """
     Returns all available skeleton structures for a given module.
     """
-    moduleObj = getattr(conf["viur.mainApp"].vi, module, None)
+    moduleObj = getattr(conf.main_app.vi, module, None)
     if not isinstance(moduleObj, Module) or not moduleObj.describe():
         return json.dumps(None)
 
@@ -67,7 +67,7 @@ def getStructure(module):
 @exposed
 @skey
 def setLanguage(lang):
-    if lang in conf["viur.availableLanguages"]:
+    if lang in conf.i18n.available_languages:
         current.language.set(lang)
 
 
@@ -75,8 +75,8 @@ def setLanguage(lang):
 def dumpConfig():
     res = {}
 
-    for key in dir(conf["viur.mainApp"].vi):
-        module = getattr(conf["viur.mainApp"].vi, key, None)
+    for key in dir(conf.main_app.vi):
+        module = getattr(conf.main_app.vi, key, None)
         if not isinstance(module, Module):
             continue
 
@@ -85,8 +85,9 @@ def dumpConfig():
 
     res = {
         "modules": res,
+        # "configuration": dict(conf.admin.items()), # TODO: this could be the short vision, if we use underscores
         "configuration": {
-            k.removeprefix("admin."): v for k, v in conf.items() if k.lower().startswith("admin.")
+            k.replace("_", "."): v for k, v in conf.admin.items()
         }
     }
     current.request.get().response.headers["Content-Type"] = "application/json"
@@ -100,13 +101,13 @@ def getVersion(*args, **kwargs):
     """
     current.request.get().response.headers["Content-Type"] = "application/json"
 
-    version = conf["viur.version"]
+    version = conf.version
 
     # always fill up to 4 parts
     while len(version) < 4:
         version += (None,)
 
-    if conf["viur.instance.is_dev_server"] \
+    if conf.instance.is_dev_server \
             or ((cuser := current.user.get()) and ("root" in cuser["access"] or "admin" in cuser["access"])):
         return json.dumps(version[:4])
 
@@ -140,9 +141,9 @@ def canAccess(*args, **kwargs) -> bool:
 
 @exposed
 def index(*args, **kwargs):
-    if not conf["viur.instance.project_base_path"].joinpath("vi", "main.html").exists():
+    if not conf.instance.project_base_path.joinpath("vi", "main.html").exists():
         raise errors.NotFound()
-    if conf["viur.instance.is_dev_server"] or current.request.get().isSSLConnection:
+    if conf.instance.is_dev_server or current.request.get().isSSLConnection:
         raise errors.Redirect("/vi/s/main.html")
     else:
         appVersion = current.request.get().request.host
@@ -154,7 +155,7 @@ def get_settings():
     fields = {key: values for key, values in conf.items()
               if key.startswith("admin.")}
 
-    fields["admin.user.google.clientID"] = conf.get("viur.user.google.clientID", "")
+    fields["admin.user.google.clientID"] = conf.user.google_client_id
 
     current.request.get().response.headers["Content-Type"] = "application/json"
     return json.dumps(fields)

src/viur/core/__init__.py
@@ -76,6 +76,8 @@ __all__ = [
     "translate",
 ]
 
+# Show DeprecationWarning from the viur-core
+warnings.filterwarnings("always", category=DeprecationWarning, module=r"viur\.core.*")
 
 def load_indexes_from_file() -> Dict[str, List]:
     """
@@ -84,7 +86,7 @@ def load_indexes_from_file() -> Dict[str, List]:
     """
     indexes_dict = {}
     try:
-        with open(os.path.join(conf["viur.instance.project_base_path"], "index.yaml"), "r") as file:
+        with open(os.path.join(conf.instance.project_base_path, "index.yaml"), "r") as file:
             indexes = yaml.safe_load(file)
             indexes = indexes.get("indexes", [])
             for index in indexes:
@@ -104,12 +106,12 @@ def setDefaultLanguage(lang: str):
 
         :param lang: Name of the language module to use by default.
     """
-    conf["viur.defaultLanguage"] = lang.lower()
+    conf.i18n.default_language = lang.lower()
 
 
 def setDefaultDomainLanguage(domain: str, lang: str):
     """
-        If conf["viur.languageMethod"] is set to "domain", this function allows setting the map of which domain
+        If conf.i18n.language_method is set to "domain", this function allows setting the map of which domain
         should use which language.
         :param domain: The domain for which the language should be set
         :param lang: The language to use (in ISO2 format, e.g. "DE")
@@ -117,21 +119,21 @@ def setDefaultDomainLanguage(domain: str, lang: str):
     host = domain.lower().strip(" /")
     if host.startswith("www."):
         host = host[4:]
-    conf["viur.domainLanguageMapping"][host] = lang.lower()
+    conf.i18n.domain_language_mapping[host] = lang.lower()
 
 
-def buildApp(modules: Union[ModuleType, object], renderers: Union[ModuleType, Dict], default: str = None):
+def buildApp(modules: Union[ModuleType, object], renderers: Union[ModuleType, Dict], default: str = None) -> Module:
     """
         Creates the application-context for the current instance.
 
         This function converts the classes found in the *modules*-module,
-        and the given renders into the object found at ``conf["viur.mainApp"]``.
+        and the given renders into the object found at ``conf.main_app``.
 
         Every class found in *modules* becomes
 
         - instanced
         - get the corresponding renderer attached
-        - will be attached to ``conf["viur.mainApp"]``
+        - will be attached to ``conf.main_app``
 
         :param modules: Usually the module provided as *modules* directory within the application.
         :param renderers: Usually the module *viur.core.renders*, or a dictionary renderName => renderClass.
@@ -207,15 +209,15 @@ def buildApp(modules: Union[ModuleType, object], renderers: Union[ModuleType, Di
             if "_postProcessAppObj" in render:  # todo: This is ugly!
                 render["_postProcessAppObj"](target)
 
-    conf["viur.mainResolver"] = resolver
+    conf.main_resolver = resolver
 
     if default in renderers and hasattr(renderers[default]["default"], "renderEmail"):
-        conf["viur.emailRenderer"] = renderers[default]["default"]().renderEmail
+        conf.emailRenderer = renderers[default]["default"]().renderEmail
     elif "html" in renderers:
-        conf["viur.emailRenderer"] = renderers["html"]["default"]().renderEmail
+        conf.emailRenderer = renderers["html"]["default"]().renderEmail
 
     # This might be useful for debugging, please keep it for now.
-    if conf["viur.debug.trace"]:
+    if conf.debug.trace:
         import pprint
         logging.debug(pprint.pformat(resolver))
 
@@ -236,20 +238,20 @@ def setup(modules: Union[object, ModuleType], render: Union[ModuleType, Dict] = 
     # noinspection PyUnresolvedReferences
     import skeletons  # This import is not used here but _must_ remain to ensure that the
     # application's data models are explicitly imported at some place!
-    if conf["viur.instance.project_id"] not in conf["viur.validApplicationIDs"]:
+    if conf.instance.project_id not in conf.valid_application_ids:
         raise RuntimeError(
-            f"""Refusing to start, {conf["viur.instance.project_id"]=} is not in {conf["viur.validApplicationIDs"]=}""")
+            f"""Refusing to start, {conf.instance.project_id=} is not in {conf.valid_application_ids=}""")
     if not render:
         import viur.core.render
         render = viur.core.render
-    conf["viur.mainApp"] = buildApp(modules, render, default)
-    # conf["viur.wsgiApp"] = webapp.WSGIApplication([(r'/(.*)', BrowseHandler)])
+    conf.main_app = buildApp(modules, render, default)
+    # conf.wsgiApp = webapp.WSGIApplication([(r'/(.*)', BrowseHandler)])
 
     # Send warning email in case trace is activated in a cloud environment
-    if ((conf["viur.debug.trace"]
-            or conf["viur.debug.traceExternalCallRouting"]
-            or conf["viur.debug.traceInternalCallRouting"])
-            and (not conf["viur.instance.is_dev_server"] or conf["viur.dev_server_cloud_logging"])):
+    if ((conf.debug.trace
+            or conf.debug.trace_external_call_routing
+            or conf.debug.trace_internal_call_routing)
+            and (not conf.instance.is_dev_server or conf.debug.dev_server_cloud_logging)):
         from viur.core import email
         try:
             email.sendEMailToAdmins(
@@ -258,33 +260,32 @@ def setup(modules: Union[object, ModuleType], render: Union[ModuleType, Dict] = 
             )
         except Exception as exc:  # OverQuota, whatever
             logging.exception(exc)
-
     # Ensure that our Content Security Policy Header Cache gets build
     from viur.core import securityheaders
     securityheaders._rebuildCspHeaderCache()
     securityheaders._rebuildPermissionHeaderCache()
     setSystemInitialized()
     # Assert that all security related headers are in a sane state
-    if conf["viur.security.contentSecurityPolicy"] and conf["viur.security.contentSecurityPolicy"]["_headerCache"]:
-        for k in conf["viur.security.contentSecurityPolicy"]["_headerCache"]:
+    if conf.security.content_security_policy and conf.security.content_security_policy["_headerCache"]:
+        for k in conf.security.content_security_policy["_headerCache"]:
             if not k.startswith("Content-Security-Policy"):
                 raise AssertionError("Got unexpected header in "
-                                     "conf['viur.security.contentSecurityPolicy']['_headerCache']")
-    if conf["viur.security.strictTransportSecurity"]:
-        if not conf["viur.security.strictTransportSecurity"].startswith("max-age"):
-            raise AssertionError("Got unexpected header in conf['viur.security.strictTransportSecurity']")
+                                     "conf.security.content_security_policy['_headerCache']")
+    if conf.security.strict_transport_security:
+        if not conf.security.strict_transport_security.startswith("max-age"):
+            raise AssertionError("Got unexpected header in conf.security.strict_transport_security")
     crossDomainPolicies = {None, "none", "master-only", "by-content-type", "all"}
-    if conf["viur.security.xPermittedCrossDomainPolicies"] not in crossDomainPolicies:
-        raise AssertionError("conf[\"viur.security.xPermittedCrossDomainPolicies\"] "
+    if conf.security.x_permitted_cross_domain_policies not in crossDomainPolicies:
+        raise AssertionError("conf.security.x_permitted_cross_domain_policies "
                              f"must be one of {crossDomainPolicies!r}")
-    if conf["viur.security.xFrameOptions"] is not None and isinstance(conf["viur.security.xFrameOptions"], tuple):
-        mode, uri = conf["viur.security.xFrameOptions"]
+    if conf.security.x_frame_options is not None and isinstance(conf.security.x_frame_options, tuple):
+        mode, uri = conf.security.x_frame_options
         assert mode in ["deny", "sameorigin", "allow-from"]
         if mode == "allow-from":
             assert uri is not None and (uri.lower().startswith("https://") or uri.lower().startswith("http://"))
     runStartupTasks()  # Add a deferred call to run all queued startup tasks
     i18n.initializeTranslations()
-    if conf["viur.file.hmacKey"] is None:
+    if conf.file_hmac_key is None:
         from viur.core import db
         key = db.Key("viur-conf", "viur-conf")
         if not (obj := db.Get(key)):  # create a new "viur-conf"?
@@ -296,7 +297,7 @@ def setup(modules: Union[object, ModuleType], render: Union[ModuleType, Dict] = 
             obj["hmacKey"] = utils.generateRandomString(length=20)
             db.Put(obj)
 
-        conf["viur.file.hmacKey"] = bytes(obj["hmacKey"], "utf-8")
+        conf.file_hmac_key = bytes(obj["hmacKey"], "utf-8")
     return app
 
 

src/viur/core/render/xml/__init__.py
@@ -21,8 +21,8 @@ def timestamp(*args, **kwargs):
 @exposed
 def dumpConfig():
     res = {}
-    for key in dir(conf["viur.mainApp"].xml):
-        module = getattr(conf["viur.mainApp"].xml, key)
+    for key in dir(conf.main_app.xml):
+        module = getattr(conf.main_app.xml, key)
         if not isinstance(module, Module):
             continue
         if admin_info := module.describe():

src/viur/core/bones/base.py
@@ -717,7 +717,7 @@ class BaseBone(object):
         elif (
             # fixme: Remove this piece of sh*t at least with VIUR4
             # We're importing from an old ViUR2 instance - there may only be keys prefixed with our name
-            conf.get("viur.viur2import.blobsource") and any(n.startswith(name + ".") for n in skel.dbEntity)
+            conf.viur2import_blobsource and any(n.startswith(name + ".") for n in skel.dbEntity)
             # ... or computed
             or self.compute
         ):
@@ -809,8 +809,8 @@ class BaseBone(object):
             res = []
             if isinstance(loadVal, dict) and "_viurLanguageWrapper_" in loadVal:
                 # Pick one language we'll use
-                if conf["viur.defaultLanguage"] in loadVal:
-                    loadVal = loadVal[conf["viur.defaultLanguage"]]
+                if conf.i18n.default_language in loadVal:
+                    loadVal = loadVal[conf.i18n.default_language]
                 else:
                     loadVal = [x for x in loadVal.values() if x is not True]
             if loadVal and not isinstance(loadVal, list):
@@ -822,8 +822,8 @@ class BaseBone(object):
             res = None
             if isinstance(loadVal, dict) and "_viurLanguageWrapper_" in loadVal:
                 # Pick one language we'll use
-                if conf["viur.defaultLanguage"] in loadVal:
-                    loadVal = loadVal[conf["viur.defaultLanguage"]]
+                if conf.i18n.default_language in loadVal:
+                    loadVal = loadVal[conf.i18n.default_language]
                 else:
                     loadVal = [x for x in loadVal.values() if x is not True]
             if loadVal and isinstance(loadVal, list):

src/viur/core/bones/boolean.py
@@ -46,7 +46,7 @@ class BooleanBone(BaseBone):
             raise ValueError("BooleanBone cannot be multiple")
 
     def singleValueFromClient(self, value, skel, bone_name, client_data):
-        return parse_bool(value, conf["viur.bone.boolean.str2true"]), None
+        return parse_bool(value, conf.bone_boolean_str2true), None
 
     def getEmptyValue(self):
         """
@@ -79,7 +79,7 @@ class BooleanBone(BaseBone):
             :param name: The property-name this bone has in its Skeleton (not the description!)
         """
         if not isinstance(skel[boneName], bool):
-            skel[boneName] = parse_bool(skel[boneName], conf["viur.bone.boolean.str2true"])
+            skel[boneName] = parse_bool(skel[boneName], conf.bone_boolean_str2true)
 
     def buildDBFilter(
         self,
@@ -101,7 +101,7 @@ class BooleanBone(BaseBone):
         :rtype: google.cloud.ndb.query.Query
         """
         if name in rawFilter:
-            val = parse_bool(rawFilter[name], conf["viur.bone.boolean.str2true"])
+            val = parse_bool(rawFilter[name], conf.bone_boolean_str2true)
             return super().buildDBFilter(name, skel, dbFilter, {name: val}, prefix=prefix)
 
         return dbFilter

src/viur/core/bones/captcha.py
@@ -26,9 +26,9 @@ class CaptchaBone(BaseBone):
         self.privateKey = privateKey
         if not self.defaultValue and not self.privateKey:
             # Merge these values from the side-wide configuration if set
-            if conf["viur.security.captcha.defaultCredentials"]:
-                self.defaultValue = self.publicKey = conf["viur.security.captcha.defaultCredentials"]["sitekey"]
-                self.privateKey = conf["viur.security.captcha.defaultCredentials"]["secret"]
+            if conf.security.captcha_default_credentials:
+                self.defaultValue = self.publicKey = conf.security.captcha_default_credentials["sitekey"]
+                self.privateKey = conf.security.captcha_default_credentials["secret"]
         self.required = True
 
     def serialize(self, skel: 'SkeletonInstance', name: str, parentIndexed: bool) -> bool:
@@ -62,7 +62,7 @@ class CaptchaBone(BaseBone):
             :param data: *User-supplied* request-data
             :returns: None or a list of errors
         """
-        if conf["viur.instance.is_dev_server"]:  # We dont enforce captchas on dev server
+        if conf.instance.is_dev_server:  # We dont enforce captchas on dev server
             return None
         if (user := current.user.get()) and "root" in user["access"]:
             return None  # Don't bother trusted users with this (not supported by admin/vi anyways)

src/viur/core/bones/date.py
@@ -232,7 +232,7 @@ class DateBone(BaseBone):
         if not (self.date and self.time and self.localize):
             return pytz.utc
 
-        if conf["viur.instance.is_dev_server"]:
+        if conf.instance.is_dev_server:
             return pytz.timezone(tzlocal.get_localzone_name())
 
         timeZone = pytz.utc  # Default fallback

src/viur/core/bones/file.py
@@ -36,7 +36,7 @@ def ensureDerived(key: db.Key, srcKey, deriveMap: Dict[str, Any], refreshKey: db
     to ensure proper relations are maintained.
     """
     from viur.core.skeleton import skeletonByKind, updateRelations
-    deriveFuncMap = conf["viur.file.derivers"]
+    deriveFuncMap = conf.file_derivations
     skel = skeletonByKind("file")()
     if not skel.fromDB(key):
         logging.info("File-Entry went missing in ensureDerived")
@@ -108,7 +108,7 @@ class FileBone(TreeLeafBone):
 
     :param derive: A set of functions used to derive other files from the referenced ones. Used fe.
         to create thumbnails / images for srcmaps from hires uploads. If set, must be a dictionary from string
-        (a key from conf["viur.file.derivers"]) to the parameters passed to that function. The parameters can be
+        (a key from conf.file_derivations) to the parameters passed to that function. The parameters can be
         any type (including None) that can be json-serialized.
 
         ..  code-block:: python
@@ -152,13 +152,13 @@ class FileBone(TreeLeafBone):
         This will always be checked against the original file uploaded - not any of it's derivatives.
         :param derive: A set of functions used to derive other files from the referenced ones.
         Used to create thumbnails and images for srcmaps from hires uploads.
-        If set, must be a dictionary from string (a key from)conf["viur.file.derivers"]) to the parameters passed to
+        If set, must be a dictionary from string (a key from) conf.file_derivations) to the parameters passed to
         that function. The parameters can be any type (including None) that can be json-serialized.
 
-            ..  code-block:: python
+        ..  code-block:: python
 
-                # Example
-                derive = {"thumbnail": [{"width": 111}, {"width": 555, "height": 666}]}
+            # Example
+            derive = {"thumbnail": [{"width": 111}, {"width": 555, "height": 666}]}
 
         :param validMimeTypes:
             A list of Mimetypes that can be selected in this bone (or None for any).
@@ -289,7 +289,7 @@ class FileBone(TreeLeafBone):
 
         from viur.core.modules.file import importBlobFromViur2
         super().refresh(skel, boneName)
-        if conf.get("viur.viur2import.blobsource"):
+        if conf.viur2import_blobsource:
             # Just ensure the file get's imported as it may not have an file entry
             val = skel[boneName]
             if isinstance(val, list):

commit 6c69860553249ec52910806068b5eaa3743e3515

fix: `vi`-render returns wrong config (regression introduced by #833) (#977)
Fix: #976
Regression fix for #833

src/viur/core/render/vi/__init__.py
@@ -152,9 +152,7 @@ def index(*args, **kwargs):
 
 @exposed
 def get_settings():
-    fields = {key: values for key, values in conf.items()
-              if key.startswith("admin.")}
-
+    fields = {key: values for key, values in conf.admin.items()}
     fields["admin.user.google.clientID"] = conf.user.google_client_id
 
     current.request.get().response.headers["Content-Type"] = "application/json"