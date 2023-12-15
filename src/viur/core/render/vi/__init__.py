ef932e08a2abe7c9f2329034cdb17c5913e42d0d
Sven Eberth <mail@sveneberth.de>
2023-12-15 14:24:57 +0100

    Update src/viur/core/render/vi/__init__.py
    Co-authored-by: Jan Max Meyer <jmm@phorward.de>

--- src/viur/core/render/vi/__init__.py
+++ src/viur/core/render/vi/__init__.py
@@ -90,9 +90,6 @@ def dumpConfig():
             k.replace("_", "."): v for k, v in conf.admin.items(True)
         }
     }
-    # The vi-admin expects moduleGroups in camelCase without "admin." prefix
-    res["configuration"]["moduleGroups"] = conf.admin.module_groups
-
     current.request.get().response.headers["Content-Type"] = "application/json"
     return json.dumps(res, cls=CustomJsonEncoder)
 
