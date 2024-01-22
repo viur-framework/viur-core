f5d0a169eba3f7477b0e90aeefcab56a788908c8
Jan Max Meyer <jmm@phorward.de>
2024-01-22 13:48:32 +0100

    Update src/viur/core/render/vi/__init__.py

--- src/viur/core/render/vi/__init__.py
+++ src/viur/core/render/vi/__init__.py
@@ -142,7 +142,7 @@ def canAccess(*args, **kwargs) -> bool:
 @exposed
 def index(*args, **kwargs):
     if args or kwargs:
-        raise errors.NotFound
+        raise errors.NotFound()
     if not conf.instance.project_base_path.joinpath("vi", "main.html").exists():
         raise errors.NotFound()
     if conf.instance.is_dev_server or current.request.get().isSSLConnection:
