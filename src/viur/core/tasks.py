viur-framework/viur-core

commit 4e4bfdec9cb25495184cd6e02a7aeb5ce54e4d50

refactor: Replace `%`-formattings by f-string (#1025)
Resolves #1024

---------

Co-authored-by: Jan Max Meyer <jmm@phorward.de>

src/viur/core/tasks.py
@@ -195,7 +195,7 @@ class TaskHandler(Module):
         self._validate_request()
         data = json.loads(req.body, object_hook=jsonDecodeObjectHook)
         if data["classID"] not in MetaQueryIter._classCache:
-            logging.error("Could not continue queryIter - %s not known on this instance" % data["classID"])
+            logging.error(f"""Could not continue queryIter - {data["classID"]} not known on this instance""")
         MetaQueryIter._classCache[data["classID"]]._qryStep(data)
 
     @exposed
@@ -226,10 +226,10 @@ class TaskHandler(Module):
             if "transactionMarker" in env:
                 marker = db.Get(db.Key("viur-transactionmarker", env["transactionMarker"]))
                 if not marker:
-                    logging.info("Dropping task, transaction %s did not apply" % env["transactionMarker"])
+                    logging.info(f"""Dropping task, transaction {env["transactionMarker"]} did not apply""")
                     return
                 else:
-                    logging.info("Executing task, transaction %s did succeed" % env["transactionMarker"])
+                    logging.info(f"""Executing task, transaction {env["transactionMarker"]} did succeed""")
             if "custom" in env and conf.tasks_custom_environment_handler:
                 # Check if we need to restore additional environmental data
                 assert isinstance(conf.tasks_custom_environment_handler, tuple) \
@@ -242,8 +242,7 @@ class TaskHandler(Module):
             pathlist = [x for x in funcPath.split("/") if x]
             for currpath in pathlist:
                 if currpath not in dir(caller):
-                    logging.error("ViUR missed a deferred task! Could not resolve the path %s. "
-                                  "Failed segment was %s", funcPath, currpath)
+                    logging.error(f"Could not resolve {funcPath=} (failed part was {currpath!r})")
                     return
                 caller = getattr(caller, currpath)
             try:
@@ -255,7 +254,7 @@ class TaskHandler(Module):
                 raise errors.RequestTimeout()  # Task-API should retry
         elif cmd == "unb":
             if not funcPath in _deferred_tasks:
-                logging.error("ViUR missed a deferred task! %s(%s,%s)", funcPath, args, kwargs)
+                logging.error(f"Missed deferred task {funcPath=} ({args=},{kwargs=})")
             # We call the deferred function *directly* (without walking through the mkDeferred lambda), so ensure
             # that any hit to another deferred function will defer again
 
@@ -274,7 +273,7 @@ class TaskHandler(Module):
         if not conf.instance.is_dev_server:
             self._validate_request(require_cron=True, require_taskname=False)
         if cronName not in _periodicTasks:
-            logging.warning("Got Cron request '%s' which doesn't have any tasks" % cronName)
+            logging.warning(f"Cron request {cronName} doesn't have any tasks")
         # We must defer from cron, as tasks will interpret it as a call originating from task-queue - causing deferred
         # functions to be called directly, wich causes calls with _countdown etc set to fail.
         req.DEFERRED_TASK_CALLED = True
@@ -283,7 +282,7 @@ class TaskHandler(Module):
             if interval:  # Ensure this task doesn't get called to often
                 lastCall = db.Get(db.Key("viur-task-interval", periodicTaskName))
                 if lastCall and utils.utcNow() - lastCall["date"] < timedelta(minutes=interval):
-                    logging.debug("Skipping task %s - Has already run recently." % periodicTaskName)
+                    logging.debug(f"Task {periodicTaskName!r} has already run recently - skipping.")
                     continue
             res = self.findBoundTask(task, conf.main_app)
             try:
@@ -292,10 +291,10 @@ class TaskHandler(Module):
                 else:
                     task()  # It seems it wasn't bound - call it as a static method
             except Exception as e:
-                logging.error("Error calling periodic task %s", periodicTaskName)
+                logging.error(f"Error calling periodic task {periodicTaskName}")
                 logging.exception(e)
             else:
-                logging.debug("Successfully called task %s", periodicTaskName)
+                logging.debug(f"Successfully called task {periodicTaskName}")
             if interval:
                 # Update its last-call timestamp
                 entry = db.Entity(db.Key("viur-task-interval", periodicTaskName))
@@ -541,12 +540,12 @@ def CallDeferred(func: t.Callable) -> t.Callable:
                 if self.__class__.__name__ == "index":
                     funcPath = func.__name__
                 else:
-                    funcPath = "%s/%s" % (self.modulePath, func.__name__)
+                    funcPath = f"{self.modulePath}/{func.__name__}"
 
                 command = "rel"
 
             except:
-                funcPath = "%s.%s" % (func.__name__, func.__module__)
+                funcPath = f"{func.__name__}.{func.__module__}"
 
                 if self is not __undefinedFlag_:
                     args = (self,) + args  # Re-append self to args, as this function is (hopefully) unbound
@@ -618,7 +617,7 @@ def CallDeferred(func: t.Callable) -> t.Callable:
             logging.info(f"Created task {func.__name__}.{func.__module__} with {args=} {kwargs=} {env=}")
 
     global _deferred_tasks
-    _deferred_tasks["%s.%s" % (func.__name__, func.__module__)] = func
+    _deferred_tasks[f"{func.__name__,}.{func.__module__}"] = func
 
     @wraps(func)
     def wrapper(*args, **kwargs):
@@ -658,7 +657,7 @@ def PeriodicTask(interval: int = 0, cronName: str = "default") -> t.Callable:
         if not cronName in _periodicTasks:
             _periodicTasks[cronName] = {}
         _periodicTasks[cronName][fn] = interval
-        fn.periodicTaskName = ("%s_%s" % (fn.__module__, fn.__qualname__)).replace(".", "_").lower()
+        fn.periodicTaskName = f"{fn.__module__}_{fn.__qualname__}".replace(".", "_").lower()
         return fn
 
     return mkDecorator
@@ -803,7 +802,7 @@ class QueryIter(object, metaclass=MetaQueryIter):
                     try:
                         doCont = cls.handleError(item, qryDict["customData"], e)
                     except Exception as e:
-                        logging.error("handleError failed on %s - bailing out" % item)
+                        logging.error(f"handleError failed on {item} - bailing out")
                         logging.exception(e)
                         doCont = False
                     if not doCont:
@@ -826,14 +825,14 @@ class QueryIter(object, metaclass=MetaQueryIter):
             Warning: If your query has an sortOrder other than __key__ and you modify that property here
             it is possible to encounter that object later one *again* (as it may jump behind the current cursor).
         """
-        logging.debug("handleEntry called on %s with %s." % (cls, entry))
+        logging.debug(f"handleEntry called on {cls} with {entry}.")
 
     @classmethod
     def handleFinish(cls, totalCount: int, customData):
         """
             Overridable hook that indicates the current run has been finished.
         """
-        logging.debug("handleFinish called on %s with %s total Entries processed" % (cls, totalCount))
+        logging.debug(f"handleFinish called on {cls} with {totalCount} total Entries processed")
 
     @classmethod
     def handleError(cls, entry, customData, exception) -> bool:
@@ -841,7 +840,7 @@ class QueryIter(object, metaclass=MetaQueryIter):
             Handle a error occurred in handleEntry.
             If this function returns True, the queryIter continues, otherwise it breaks and prints the current cursor.
         """
-        logging.debug("handleError called on %s with %s." % (cls, entry))
+        logging.debug(f"handleError called on {cls} with {entry}.")
         logging.exception(exception)
         return True
 

src/viur/core/bones/base.py
@@ -385,9 +385,9 @@ class BaseBone(object):
             res = {}
             for lang in languages:
                 if not collectSubfields:
-                    if "%s.%s" % (name, lang) in data:
+                    if f"{name}.{lang}" in data:
                         fieldSubmitted = True
-                        res[lang] = data["%s.%s" % (name, lang)]
+                        res[lang] = data[f"{name}.{lang}"]
                         if multiple and not isinstance(res[lang], list):
                             res[lang] = [res[lang]]
                         elif not multiple and isinstance(res[lang], list):
@@ -397,9 +397,9 @@ class BaseBone(object):
                                 res[lang] = None
                 else:
                     for key in data.keys():  # Allow setting relations with using, multiple and languages back to none
-                        if key == "%s.%s" % (name, lang):
+                        if key == f"{name}.{lang}":
                             fieldSubmitted = True
-                    prefix = "%s.%s." % (name, lang)
+                    prefix = f"{name}.{lang}."
                     if multiple:
                         tmpDict = {}
                         for key, value in data.items():
@@ -446,7 +446,7 @@ class BaseBone(object):
                 for key in data.keys():  # Allow setting relations with using, multiple and languages back to none
                     if key == name:
                         fieldSubmitted = True
-                prefix = "%s." % name
+                prefix = f"{name}."
                 if multiple:
                     tmpDict = {}
                     for key, value in data.items():
@@ -794,7 +794,7 @@ class BaseBone(object):
             else:  # We could not parse this, maybe it has been written before languages had been set?
                 for language in self.languages:
                     res[language] = None
-                    oldKey = "%s.%s" % (name, language)
+                    oldKey = f"{name}.{language}"
                     if oldKey in skel.dbEntity and skel.dbEntity[oldKey]:
                         res[language] = self.singleValueUnserialize(skel.dbEntity[oldKey])
                         loadVal = None  # Don't try to import later again, this format takes precedence
@@ -937,7 +937,7 @@ class BaseBone(object):
             if inEqFilter:
                 inEqFilter = inEqFilter[0][: inEqFilter[0].find(" ")]
                 if inEqFilter != order[0]:
-                    logging.warning("I fixed you query! Impossible ordering changed to %s, %s" % (inEqFilter, order[0]))
+                    logging.warning(f"I fixed you query! Impossible ordering changed to {inEqFilter}, {order[0]}")
                     dbFilter.order((inEqFilter, order))
                 else:
                     dbFilter.order(order)
@@ -963,19 +963,19 @@ class BaseBone(object):
             h.update(str(value).encode("UTF-8"))
             res = h.hexdigest()
             if isinstance(value, int) or isinstance(value, float):
-                return "I-%s" % res
+                return f"I-{res}"
             elif isinstance(value, str):
-                return "S-%s" % res
+                return f"S-{res}"
             elif isinstance(value, db.Key):
                 # We Hash the keys here by our self instead of relying on str() or to_legacy_urlsafe()
                 # as these may change in the future, which would invalidate all existing locks
                 def keyHash(key):
                     if key is None:
                         return "-"
-                    return "%s-%s-<%s>" % (hashValue(key.kind), hashValue(key.id_or_name), keyHash(key.parent))
+                    return f"{hashValue(key.kind)}-{hashValue(key.id_or_name)}-<{keyHash(key.parent)}>"
 
-                return "K-%s" % keyHash(value)
-            raise NotImplementedError("Type %s can't be safely used in an uniquePropertyIndex" % type(value))
+                return f"K-{keyHash(value)}"
+            raise NotImplementedError(f"Type {type(value)} can't be safely used in an uniquePropertyIndex")
 
         if not value and not self.unique.lockEmpty:
             return []  # We are zero/empty string and these should not be locked
@@ -1068,8 +1068,8 @@ class BaseBone(object):
         if getattr(otherSkel, boneName) is None:
             return
         if not isinstance(getattr(otherSkel, boneName), type(self)):
-            logging.error("Ignoring values from conflicting boneType (%s is not a instance of %s)!" % (
-                getattr(otherSkel, boneName), type(self)))
+            logging.error(f"Ignoring values from conflicting boneType ({getattr(otherSkel, boneName)} is not a "
+                          f"instance of {type(self)})!")
             return
         valuesCache[boneName] = copy.deepcopy(otherSkel.valuesCache.get(boneName, None))
 

src/viur/core/bones/date.py
@@ -286,7 +286,7 @@ class DateBone(BaseBone):
             if self.naive:
                 value = value.replace(tzinfo=timezone.utc)
             # We should always deal with timezone aware datetimes
-            assert value.tzinfo, "Encountered a naive Datetime object in %s - refusing to save." % name
+            assert value.tzinfo, f"Encountered a naive Datetime object in {name} - refusing to save."
         return value
 
     def singleValueUnserialize(self, value):

src/viur/core/bones/file.py
@@ -48,11 +48,11 @@ def ensureDerived(key: db.Key, srcKey, deriveMap: dict[str, t.Any], refreshKey: 
     skel["derived"]["files"] = skel["derived"].get("files") or {}
     resDict = {}  # Will contain new or updated resultDicts that will be merged into our file
     for calleeKey, params in deriveMap.items():
-        fullSrcKey = "%s_%s" % (srcKey, calleeKey)
+        fullSrcKey = f"{srcKey}_{calleeKey}"
         paramsHash = sha256(str(params).encode("UTF-8")).hexdigest()  # Hash over given params (dict?)
         if skel["derived"]["deriveStatus"].get(fullSrcKey) != paramsHash:
             if calleeKey not in deriveFuncMap:
-                logging.warning("File-Deriver %s not found - skipping!" % calleeKey)
+                logging.warning(f"File-Deriver {calleeKey} not found - skipping!")
                 continue
             callee = deriveFuncMap[calleeKey]
             callRes = callee(skel, skel["derived"]["files"], params)
@@ -221,7 +221,7 @@ class FileBone(TreeLeafBone):
             if isinstance(values, dict):
                 values = [values]
             for val in values:  # Ensure derives getting build for each file referenced in this relation
-                ensureDerived(val["dest"]["key"], "%s_%s" % (skel.kindName, boneName), self.derive)
+                ensureDerived(val["dest"]["key"], f"{skel.kindName}_{boneName}", self.derive)
 
         values = skel[boneName]
         if self.derive and values:

src/viur/core/bones/key.py
@@ -191,7 +191,7 @@ class KeyBone(BaseBone):
                     return db.Key.from_legacy_urlsafe(key)
                 except Exception as e:
                     logging.exception(e)
-                    logging.warning("Could not decode key %s" % key)
+                    logging.warning(f"Could not decode key {key}")
                     raise RuntimeError()
 
         if name in rawFilter:
@@ -206,18 +206,18 @@ class KeyBone(BaseBone):
                     newFilter = copy.deepcopy(oldFilter)
                     try:
                         if name == "key":
-                            newFilter.filters["%s%s =" % (prefix or "", db.KEY_SPECIAL_PROPERTY)] = _decodeKey(key)
+                            newFilter.filters[f"{prefix or ''}{db.KEY_SPECIAL_PROPERTY} ="] = _decodeKey(key)
                         else:
-                            newFilter.filters["%s%s =" % (prefix or "", name)] = _decodeKey(key)
+                            newFilter.filters[f"{prefix or ''}{name} ="] = _decodeKey(key)
                     except:  # Invalid key or something
                         raise RuntimeError()
                     dbFilter.queries.append(newFilter)
             else:
                 try:
                     if name == "key":
-                        dbFilter.filter("%s%s =" % (prefix or "", db.KEY_SPECIAL_PROPERTY), _decodeKey(rawFilter[name]))
+                        dbFilter.filter(f"""{prefix or ""}{db.KEY_SPECIAL_PROPERTY} =""", _decodeKey(rawFilter[name]))
                     else:
-                        dbFilter.filter("%s%s =" % (prefix or "", name), _decodeKey(rawFilter[name]))
+                        dbFilter.filter(f"""{prefix or ""}{name} =""", _decodeKey(rawFilter[name]))
                 except:  # Invalid key or something
                     raise RuntimeError()
             return dbFilter

src/viur/core/bones/numeric.py
@@ -156,7 +156,7 @@ class NumericBone(BaseBone):
                         paramValue = float(paramValue)
                 except ValueError:
                     # The value we should filter by is garbage, cancel this query
-                    logging.warning("Invalid filtering! Unparsable int/float supplied to NumericBone %s" % name)
+                    logging.warning(f"Invalid filtering! Unparsable int/float supplied to NumericBone {name}")
                     raise RuntimeError()
                 updatedFilter[parmKey] = paramValue
 

src/viur/core/bones/randomslice.py
@@ -110,13 +110,13 @@ class RandomSliceBone(BaseBone):
                 rndVal = random()  # Choose our Slice center
                 # Right Side
                 q = db.QueryDefinition(origKind, {}, [])
-                property, value = applyFilterHook(dbFilter, "%s <=" % name, rndVal)
+                property, value = applyFilterHook(dbFilter, f"{name} <=", rndVal)
                 q.filters[property] = value
                 q.orders = [(name, db.SortOrder.Descending)]
                 queries.append(q)
                 # Left Side
                 q = db.QueryDefinition(origKind, {}, [])
-                property, value = applyFilterHook(dbFilter, "%s >" % name, rndVal)
+                property, value = applyFilterHook(dbFilter, f"{name} >", rndVal)
                 q.filters[property] = value
                 q.orders = [(name, db.SortOrder.Ascending)]
                 queries.append(q)

src/viur/core/bones/record.py
@@ -62,7 +62,7 @@ class RecordBone(BaseBone):
             return None
         elif isinstance(value, list) and value:
             value = value[0]
-        assert isinstance(value, dict), "Read something from the datastore thats not a dict: %s" % str(type(value))
+        assert isinstance(value, dict), f"Read something from the datastore thats not a dict: {type(value)}"
         usingSkel = self.using()
         usingSkel.unserialize(value)
         return usingSkel
@@ -142,7 +142,7 @@ class RecordBone(BaseBone):
             return res
         uskel = self.using()
         for idx, val in enumerate(value):
-            getValues(res, uskel, val, "%s%s_%s" % (prefix, name, str(idx)))
+            getValues(res, uskel, val, f"{prefix}{name}_{idx}")
 
         return res
 

commit 0a10ca9fed64951014e7b70c4ffa40ccd5742c33

fix: f-string in CallDeferred (caused by #1025)
must be string, not tuple

src/viur/core/tasks.py
@@ -617,7 +617,7 @@ def CallDeferred(func: t.Callable) -> t.Callable:
             logging.info(f"Created task {func.__name__}.{func.__module__} with {args=} {kwargs=} {env=}")
 
     global _deferred_tasks
-    _deferred_tasks[f"{func.__name__,}.{func.__module__}"] = func
+    _deferred_tasks[f"{func.__name__}.{func.__module__}"] = func
 
     @wraps(func)
     def wrapper(*args, **kwargs):