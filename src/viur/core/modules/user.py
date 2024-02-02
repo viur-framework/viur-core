352dd5e9b2cf37a776c27839cb393bf368355595
Jan Max Meyer <jmm@phorward.de>
2024-02-02 16:13:35 +0100

    Apply suggestions from code review
    Co-authored-by: Sven Eberth <mail@sveneberth.de>

--- src/viur/core/modules/user.py
+++ src/viur/core/modules/user.py
@@ -185,7 +185,7 @@ class UserAuthentication(Module, abc.ABC):
         return True
 
     @classmethod
-    def patch_user_skel(cls, skel_cls: skeleton.Skeleton):
+    def patch_user_skel(cls, skel_cls: skeleton.Skeleton) -> skeleton.Skeleton:
         """
         Allows for an UserAuthentication to patch the UserSkel
         class with additional bones which are required for
