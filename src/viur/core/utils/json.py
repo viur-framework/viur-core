b573111cd7b8ff7522c393099993b220dae426a3
Jan Max Meyer <jmm@phorward.de>
2024-02-16 16:53:00 +0100

    Replace _preprocess_json_object by ViURJsonEncoder

--- src/viur/core/utils/json.py
+++ src/viur/core/utils/json.py
@@ -6,36 +6,37 @@ import typing as t
 from viur.core import db
 
 
-def _preprocess_json_object(obj: t.Any) -> t.Any:
+class ViURJsonEncoder(json.JSONEncoder):
     """
     Adds support for db.Key, db.Entity, datetime, bytes and and converts the provided obj
     into a special dict with JSON-serializable values.
     """
-    if isinstance(obj, db.Key):
-        return {".__key__": db.encodeKey(obj)}
-    elif isinstance(obj, datetime.datetime):
-        return {".__datetime__": obj.astimezone(pytz.UTC).isoformat()}
-    elif isinstance(obj, bytes):
-        return {".__bytes__": base64.b64encode(obj).decode("ASCII")}
-    elif isinstance(obj, db.Entity):
-        # TODO: Support Skeleton instances as well?
-        return {
-            ".__entity__": _preprocess_json_object(dict(obj)),
-            ".__ekey__": db.encodeKey(obj.key) if obj.key else None
-        }
-    elif isinstance(obj, dict):
-        return {_preprocess_json_object(k): _preprocess_json_object(v) for k, v in obj.items()}
-    elif isinstance(obj, (list, tuple, set)):
-        return [_preprocess_json_object(x) for x in obj]
+    def default(self, obj: t.Any) -> t.Any:
+        if isinstance(obj, db.Key):
+            return {".__key__": db.encodeKey(obj)}
+        elif isinstance(obj, datetime.datetime):
+            return {".__datetime__": obj.astimezone(pytz.UTC).isoformat()}
+        elif isinstance(obj, bytes):
+            return {".__bytes__": base64.b64encode(obj).decode("ASCII")}
+        elif isinstance(obj, db.Entity):
+            # TODO: Handle SkeletonInstance as well?
+            return {
+                ".__entity__": self.default(dict(obj)),
+                ".__key__": db.encodeKey(obj.key) if obj.key else None
+            }
+        elif isinstance(obj, dict):
+            return {self.default(key): self.default(value) for key, value in obj.items()}
+        elif isinstance(obj, (list, tuple, set)):
+            return (self.default(item) for item in obj)
 
-    return obj
+        return super().default(obj)
 
 
-def dumps(obj: t.Any, *args, **kwargs) -> str:
+def dumps(obj: t.Any, *, cls=ViURJsonEncoder, **kwargs) -> str:
     """
     Wrapper for json.dumps() which converts additional ViUR datatypes.
     """
-    return json.dumps(_preprocess_json_object(obj), *args, **kwargs)
+    return json.dumps(obj, cls=cls, **kwargs)
 
 
 def _decode_object_hook(obj: t.Any):
@@ -46,21 +47,24 @@ def _decode_object_hook(obj: t.Any):
     if len(obj) == 1:
         if key := obj.get(".__key__"):
             return db.Key.from_legacy_urlsafe(key)
-        elif date := obj.get(".__datetime__"):
+
+        if date := obj.get(".__datetime__"):
             return datetime.datetime.fromisoformat(date)
-        elif buf := obj.get(".__bytes__"):
+
+        if buf := obj.get(".__bytes__"):
             return base64.b64decode(buf)
 
-    elif len(obj) == 2 and ".__entity__" in obj and ".__ekey__" in obj:
-        entity = db.Entity(db.Key.from_legacy_urlsafe(obj[".__ekey__"]) if obj[".__ekey__"] else None)
+    elif len(obj) == 2 and all(k in obj for k in (".__entity__", ".__key__")):
+        # TODO: Handle SkeletonInstance as well?
+        entity = db.Entity(db.Key.from_legacy_urlsafe(obj[".__key__"]) if obj[".__key__"] else None)
         entity.update(obj[".__entity__"])
         return entity
 
     return obj
 
 
-def loads(s: str) -> t.Any:
+def loads(s: str, *, object_hook=_decode_object_hook, **kwargs) -> t.Any:
     """
     Wrapper for json.loads() which recreates additional ViUR datatypes.
     """
-    return json.loads(s, object_hook=_decode_object_hook)
+    return json.loads(s, object_hook=object_hook, **kwargs)
