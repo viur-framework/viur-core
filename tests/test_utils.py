b871e6c2a4f108fa4b51822f03d2e31d71ef4256
Jan Max Meyer <jmm@phorward.de>
2024-02-29 17:55:35 +0100

    feat: `utils.json` module / improving `JsonBone` (#1072)
    Proposal to fix #1064.
    
    I'm still a little unhappy with this, as it bites with #1000, but after
    all it's a different use case. Maybe we can bring both together in some
    way.
    
    ---------
    
    Co-authored-by: Sven Eberth <mail@sveneberth.de>

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -25,3 +25,47 @@ class TestUtils(unittest.TestCase):
         self.assertEqual("abcde", utils.string.escape("abcdefghi", max_length=5))
         self.assertEqual("&lt;html&gt; &&lt;/html&gt;", utils.string.escape("<html>\n&\0</html>"))
         self.assertEqual(utils.string.escape(S), E)
+
+    def test_json(self):
+        from viur.core import utils, db
+        import datetime
+
+        # key = db.Key("test", "hello world")
+        now = datetime.datetime.fromisoformat("2024-02-28T14:43:17.125207+00:00")
+        duration = datetime.timedelta(minutes=13, microseconds=37)
+
+        example = {
+            "datetime": now,
+            "false": False,
+            "float": 42.5,
+            "generator": (x for x in "Hello"),
+            "int": 1337,
+            # "key": key, # cannot use in tests
+            "list": [1, 2, 3],
+            "none": None,
+            "set": {1, 2, 3},
+            "str": "World",
+            "timedelta": duration,
+            "true": True,
+            "tuple": (1, 2, 3),
+        }
+
+        # serialize example into string
+        s = utils.json.dumps(example)
+
+        # check if string is as expected
+        self.assertEqual(
+            s,
+            """{"datetime": {".__datetime__": "2024-02-28T14:43:17.125207+00:00"}, "false": false, "float": 42.5, "generator": ["H", "e", "l", "l", "o"], "int": 1337, "list": [1, 2, 3], "none": null, "set": {".__set__": [1, 2, 3]}, "str": "World", "timedelta": {".__timedelta__": 780000037.0}, "true": true, "tuple": [1, 2, 3]}""",  # noqa
+        )
+
+        # deserialize string into object again
+        o = utils.json.loads(s)
+
+        # patch tuple as a list
+        example["tuple"] = list(example["tuple"])
+        example["generator"] = [x for x in "Hello"]
+
+        # self.assertEqual(example, o)
+        for k, v in example.items():
+            self.assertEqual(o[k], v)
