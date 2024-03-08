ca9f935832c14d7777e17f758fac0b33116782f6
Sven Eberth <se@mausbrand.de>
2024-03-08 21:52:26 +0100

    feat: Implement `utils.parse.timedelta` (#1086)

--- src/viur/core/utils/parse.py
+++ src/viur/core/utils/parse.py
@@ -2,6 +2,7 @@
 ViUR utility functions regarding parsing.
 """
 import typing as t
+import datetime
 
 
 def bool(value: t.Any, truthy_values: t.Iterable[str] = ("true", "yes", "1")) -> bool:
@@ -17,3 +18,19 @@ def bool(value: t.Any, truthy_values: t.Iterable[str] = ("true", "yes", "1")) ->
     :returns: True if the value matches any of the truthy values, False otherwise.
     """
     return str(value).strip().lower() in truthy_values
+
+
+def timedelta(value: datetime.timedelta | int | float | str) -> datetime.timedelta:
+    """
+    Parse a value into a timedelta object.
+
+    This method takes a seconds value and converts it into
+    a timedelta object, if it is not already one.
+    :param value: The value to be parsed into a timedelta.
+    :returns: A timedelta object.
+    """
+    if isinstance(value, datetime.timedelta):
+        return value
+    if isinstance(value, str):
+        value = float(value)
+    return datetime.timedelta(seconds=value)
