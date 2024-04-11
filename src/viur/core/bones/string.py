f404df67ae62e70b0748916be70e10448c4cde32
Arne Gudermann <ag@mausbrand.de>
2024-04-11 11:03:18 +0200

    Readd Stringbone max_length check

--- src/viur/core/bones/string.py
+++ src/viur/core/bones/string.py
@@ -130,10 +130,9 @@ class StringBone(BaseBone):
         Returns None and the escaped value if the value would be valid for
         this bone, otherwise the empty value and an error-message.
         """
-        value = utils.string.escape(value, self.max_length)
 
         if not (err := self.isInvalid(value)):
-            return value, None
+            return utils.string.escape(value, self.max_length), None
 
         return self.getEmptyValue(), [ReadFromClientError(ReadFromClientErrorSeverity.Invalid, err)]
 
