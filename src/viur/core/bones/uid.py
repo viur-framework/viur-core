c0fbcaa6edbe1a7fa3e293f6c12149bcba5e2802
Arne Gudermann <ag@mausbrand.de>
2024-04-16 12:07:08 +0200

    Rework generate_uid

--- src/viur/core/bones/uid.py
+++ src/viur/core/bones/uid.py
@@ -12,20 +12,29 @@ from viur.core.bones.base import BaseBone, Compute, ComputeInterval, ComputeMeth
     UniqueLockMethod
 
 
-def generate_uid(bone):
-    if "*" in bone.pattern and "?" not in bone.pattern and "#" not in bone.pattern:
-        return bone.pattern.replace("*", utils.string.random(bone.length))
-    res = bone.pattern
-    remaining_chars = bone.length
-    if "?" in bone.pattern:
-        while "?" in res:
-            remaining_chars -= 1
-            res = res.replace("?", utils.string.random(1), 1)
-    if "#" in bone.pattern:
-        while "#" in res:
-            remaining_chars -= 1
-            res = res.replace("#", secrets.choice(string.digits), 1)
-    return res.replace("*", utils.string.random(remaining_chars))
+def generate_uid(skel, bone):
+    def transac(_key):
+
+        if db_obj := db.Get(_key):
+            db_obj["count"] += 1
+            db.Put(db_obj)
+        else:
+            db_obj = db.Entity(_key)
+            db_obj["count"] = 0
+            db.Put(db_obj)
+        return db_obj["count"]
+
+    db_key = db.Key("viur-uids", f"{skel.kindName}-{bone.name}-uid")
+    if db.IsInTransaction():
+        count_value = transac(db_key)
+    else:
+        count_value = db.RunInTransaction(transac, db_key)
+    if bone.fill_chars:
+        length_to_fill = bone.length - len(bone.pattern)
+        res = str(count_value).rjust(length_to_fill, bone.fill_chars)
+        return bone.pattern.replace("*", res)
+    else:
+        return bone.pattern.replace("*", str(count_value))
 
 
 class UidBone(BaseBone):
@@ -37,8 +46,9 @@ class UidBone(BaseBone):
     def __init__(
         self,
         *,
+        fill_chars="",
         length: int | None = 13,
-        pattern: str | None = "*",
+        pattern: str | t.Callable | None = "*",
         compute: Compute = Compute(fn=generate_uid, interval=ComputeInterval(ComputeMethod.Once)),
         unique=UniqueValue(UniqueLockMethod.SameValue, False, "Unique Value already in use"),
         **kwargs
@@ -60,8 +70,13 @@ class UidBone(BaseBone):
             self.readOnly = True
             # raise ValueError("UidBone must be readOnly")
 
+        self.fill_chars = str(fill_chars)
         self.length = length
-        self.pattern = pattern
+        if isinstance(pattern, t.Callable):
+            pattern = pattern()
+        self.pattern = str(pattern)
+        if self.pattern.count("*") != 1:
+            raise ValueError("Only one Wildcard (*) is allowed in the pattern")
 
     def structure(self) -> dict:
         ret = super().structure() | {
