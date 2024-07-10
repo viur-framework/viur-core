9764e4b4ada7d5c991d1327e8f8946c21afc6541
Jan Max Meyer <jmm@phorward.de>
2024-07-10 15:14:07 +0200

    Finalizing standard SpamBone
    (tested with language default "en" and "de")

--- src/viur/core/bones/spam.py
+++ src/viur/core/bones/spam.py
@@ -1,39 +1,66 @@
-import random, logging
-from viur.core import utils, i18n, current
+import logging
+import random
+import typing as t
+from viur.core import i18n, current
 from viur.core.bones import NumericBone
 
 
 class SpamBone(NumericBone):
+    type = "numeric.spam"
 
-    def __init__(self, required=True, precision=0, *args, **kwargs):
-        super().__init__(required=required, precision=0, *args, **kwargs)
-        self.defaultNumbers = ["eins", "zwei", "drei", "vier", "fÃ¼nf", "sechs", "sieben", "acht", "neun"]
+    def __init__(
+        self,
+        descr: str = i18n.translate(
+            "core.bones.spam.question",
+            "What is the result of the addition of {{a}} and {{b}}?"
+        ),
+        values: t.Iterable[str] = (
+            i18n.translate(f"core.bones.spam.value.{digit}", digit)
+            for digit in ("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+        ),
+        required: bool = True,
+        precision: int = 0,
+        msg_invalid: str = i18n.translate(
+            "core.bones.spam.invalid",
+            "Your answer was wrong! Please try again."
+        ),
+        **kwargs
+    ):
+        if precision != 0:
+            raise ValueError(f"Cannot use {self.__class__.__name__!r} with a precision")
 
-    def _getRandomNumber(self):
+        super().__init__(
+            required=required,
+            precision=precision,
+            **kwargs
+        )
+
+        self.descr_template = descr
+        self.values = tuple(values)
+        self.msg_invalid = msg_invalid
+
+    def _dice(self):
         num = 0
         while num == 0:
-            num = int(random.random() * 10)
+            num = int(random.random() * len(self.values) + 1)
 
         return num
 
     @property
     def descr(self):
+        """
+        The descr-property is generated and uses session variables to construct the question.
+        """
         session = current.session.get()
-        a = session.get("spamBone.a")
-        b = session.get("spamBone.b")
+
+        a = session.get("spambone.value.a")
+        b = session.get("spambone.value.b")
 
         if a is None or b is None:
-            a = session["spamBone.a"] = self._getRandomNumber()
-            b = session["spamBone.b"] = self._getRandomNumber()
-            session.markChanged()
+            a = session["spambone.value.a"] = self._dice()
+            b = session["spambone.value.b"] = self._dice()
 
-        return i18n.translate(
-            "spambone.confirm",
-            "<strong>SPAM * - Addiere {{a}} plus {{b}}. Antwort als Ziffer.</strong>"
-        ).translate(
-            a=i18n.translate("spambone.%d" % a, self.defaultNumbers[a - 1]).translate(),
-            b=i18n.translate("spambone.%d" % b, self.defaultNumbers[b - 1]).translate()
-        )
+        return i18n.translate(self.descr_template).translate(a=self.values[a - 1], b=self.values[b - 1])
 
     @descr.setter
     def descr(self, value):
@@ -42,19 +69,18 @@ class SpamBone(NumericBone):
     def isInvalid(self, value):
         session = current.session.get()
 
-        a = session.get("spamBone.a") or 0
-        b = session.get("spamBone.b") or 0
+        a = session.get("spambone.value.a") or 0
+        b = session.get("spambone.value.b") or 0
 
         if a and b:
-            del session["spamBone.a"]
-            del session["spamBone.b"]
-            session.markChanged()
+            del session["spambone.value.a"]
+            del session["spambone.value.b"]
 
             try:
                 value = int(value)
-            except:
-                return False
+            except ValueError:
+                value = 0
 
-        logging.debug("a=%r, b=%r, value=%r, expecting=%r", a, b, value, a+b)
+        logging.debug(f"{a=}, {b=}, {value=}, expecting {a + b=}")
         if value != a + b:
-            return i18n.translate("spambone.invalid", "Deine Antwort war falsch. Bitte versuche es erneut.").translate()
+            return str(self.msg_invalid)
