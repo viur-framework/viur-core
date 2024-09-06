c1f471d356bf3deecf7f4ced73b9ecdc5143c0e2
Arne Gudermann <ag@mausbrand.de>
2024-09-06 13:50:55 +0200

    Add more Doctring

--- src/viur/core/bones/uri.py
+++ src/viur/core/bones/uri.py
@@ -12,34 +12,41 @@ class URIBone(BaseBone):
 
        :param accepted_protocols: The accepted protocols can be set to allow only the provide protocols.
        :param accepted_ports The accepted ports can be set to allow only the provide ports.
+       ..  code-block:: python
+            # Example
+            URIBone(accepted_ports=1)
+            URIBone(accepted_ports="2")
+            URIBone(accepted_ports="1-4")
+            URIBone(accepted_ports=[1,"2","4-10"])
        :param clean_get_params: When set to Ture the GET Parameter for the URL will be cleand.
        :param domain_allowed_list: If set only the URLs that are match with an entry of this list will be accepted.
        :param domain_disallowed_list: If set only the URLs that are not match with an entry of this list will be accepted.
+       :param local_allowed_list: If True the URLs that are local paths will be prefixed with "/".
        """
     type = "uri"
 
     def __init__(
         self,
         *,
-        accepted_protocols: str | list[str] = "*",
-        accepted_ports: int | str | list[int] | list[str] = "*",
+        accepted_protocols: str | t.Iterable[str] | None = None,
+        accepted_ports: int | str | t.Iterable[int] | t.Iterable[str] | None = None,
         clean_get_params: bool = False,
-        domain_allowed_list: list[str] | None = None,
-        domain_disallowed_list: list[str] | None = None,
+        domain_allowed_list: t.Iterable[str] | None = None,
+        domain_disallowed_list: t.Iterable[str] | None = None,
         local_path_allowed: bool = False,
         **kwargs
     ):
         super().__init__(**kwargs)
         self.accepted_ports = self.build_accepted_ports(accepted_ports)
         if "*" in self.accepted_ports:
-            self.accepted_ports = "*"
+            self.accepted_ports = None
 
         self.accepted_protocols = accepted_protocols
-        if not isinstance(self.accepted_protocols, Iterable) or isinstance(self.accepted_protocols, str):
-            self.accepted_protocols = [self.accepted_protocols]
-
-        if "*" in accepted_protocols:
-            self.accepted_protocols = "*"
+        if self.accepted_protocols:
+            if not isinstance(self.accepted_protocols, Iterable) or isinstance(self.accepted_protocols, str):
+                self.accepted_protocols = set(self.accepted_protocols)
+            if "*" in accepted_protocols:
+                self.accepted_protocols = None
 
         if not isinstance(clean_get_params, bool):
             raise ValueError("clean_get_params must be a boolean")
@@ -70,7 +77,7 @@ class URIBone(BaseBone):
         self.local_path_allowed = local_path_allowed
 
     def build_accepted_ports(self, accepted_ports):
-        accepted_ports_value = []
+        accepted_ports_value = set()
         if isinstance(accepted_ports, str):
             if accepted_ports == "*":
                 return "*"
@@ -87,24 +94,30 @@ class URIBone(BaseBone):
                         if start < 0:
                             raise ValueError("Start value must be greater than zero")
 
-                        if end < 65535:  # 2**16 max ports:
+                        if end > 65535:  # 2**16 max ports:
                             raise ValueError("End value must be less or equal than 65535")
 
-                        accepted_ports_value.extend(range(start, end + 1))
+                        accepted_ports_value.update(range(start, end + 1))
 
                     else:
-                        accepted_ports_value.append(int(port))
+                        accepted_ports_value.add(int(port))
                 return accepted_ports_value
         if isinstance(accepted_ports, int):
-            accepted_ports_value.append(accepted_ports)
+            if accepted_ports < 0:
+                raise ValueError("Port value must be greater than zero")
+
+            if accepted_ports > 65535:  # 2**16 max ports:
+                raise ValueError("Port value must be less or equal than 65535")
+
+            accepted_ports_value.add(accepted_ports)
             return accepted_ports_value
 
-        if isinstance(accepted_ports, (list, tuple)):
+        if isinstance(accepted_ports, Iterable):
             for accepted_port in accepted_ports:
-                accepted_ports_value.extend(self.build_accepted_ports(accepted_port))
+                accepted_ports_value |= self.build_accepted_ports(accepted_port)
             return accepted_ports_value
 
-        raise ValueError("accepted_ports must be a list or a tuple or an integer or string")
+        raise ValueError("accepted_ports must be a iterable or an integer or string")
 
     def isInvalid(self, value):
         try:
@@ -115,11 +128,11 @@ class URIBone(BaseBone):
         if not self.local_path_allowed and parsed_url.scheme == "":
             return f"""No protocol specified"""
 
-        if self.accepted_ports != "*":
+        if self.accepted_ports:
             if parsed_url.port not in self.accepted_ports:
                 return f""""{parsed_url.port}" not in the accepted ports."""
 
-        if self.accepted_protocols != "*":
+        if self.accepted_protocols:
             for protocol in self.accepted_protocols:
                 if fnmatch.fnmatch(parsed_url.scheme, protocol):
                     break
@@ -144,7 +157,7 @@ class URIBone(BaseBone):
 
         parsed_url = urlparse(value)
         if self.local_path_allowed and parsed_url.scheme == "":
-            if value[0] not in ["?", "#", "/"]:
+            if value[0] not in "?#/":
                 value = f"/{value}"
                 parsed_url = urlparse(value)
 
@@ -169,8 +182,8 @@ class URIBone(BaseBone):
 
     def structure(self) -> dict:
         return super().structure() | {
-            "accepted_protocols": self.accepted_protocols,
-            "accepted_ports": self.accepted_ports,
+            "accepted_protocols": list(self.accepted_protocols) if self.accepted_protocols else None,
+            "accepted_ports": list(self.accepted_ports) if self.accepted_ports else None,
             "clean_get_params": self.clean_get_params,
             "domain_allowed_list": self.domain_allowed_list,
             "domain_disallowed_list": self.domain_disallowed_list,
