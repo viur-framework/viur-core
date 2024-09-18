e65cb975de11952484efcfea2f3f25fc7a10e0b1
Kadir Balku <KadirBalku@gmail.com>
2024-09-19 00:28:23 +0200

    chore: pep8

--- src/viur/core/bones/phone.py
+++ src/viur/core/bones/phone.py
@@ -1,13 +1,11 @@
 import re
 import typing as t
-import json
 
 from viur.core.bones.string import StringBone
 from viur.core.bones.base import ReadFromClientError, ReadFromClientErrorSeverity
 
-from pathlib import Path
-
 DEFAULT_REGEX = r"^\+?(\d{1,3})[-\s]?(\d{1,4})[-\s]?(\d{1,4})[-\s]?(\d{1,9})$"
+
 
 class PhoneBone(StringBone):
     """
