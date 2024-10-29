4cb5ea667a2c56063db111d622bda3edd70166de
Arne Gudermann <ag@mausbrand.de>
2024-10-29 08:27:32 +0100

    Change Checks

--- src/viur/core/modules/file.py
+++ src/viur/core/modules/file.py
@@ -1175,22 +1175,21 @@ class File(Tree):
         :param key: The key of the file
         :param dlkey: The download key of the file
         :param filename: The filename to be given. If no filename is provided
-        downloadUrls for all derived files are returned in case of `derived=True`.
+            downloadUrls for all derived files are returned in case of `derived=True`.
         :param derived: True, if a derived file download URL is being requested.
         """
-        if key is not None:
-            skel = self.viewSkel("leaf")
-        elif dlkey is not None:
-            skel = self.viewSkel("leaf").all().filter("dlkey", dlkey).getSkel()
-        else:
+        skel = self.viewSkel("leaf")
+        if dlkey is not None:
+            skel = skel.all().filter("dlkey", dlkey).getSkel()
+        elif key is None and dlkey is None:
             raise errors.BadRequest("No key or dlkey provided")
-            
+
         if not (skel and skel.read(key)):
             raise errors.NotFound()
 
         if not self.canView("leaf", skel):
             raise errors.Unauthorized()
-      
+
         dlkey = skel["dlkey"]
 
         if derived and filename is None:
@@ -1198,6 +1197,16 @@ class File(Tree):
             for filename in skel["derived"]["files"]:
                 res[filename] = {"downloadUrl": self.create_download_url(dlkey, filename, derived)}
         else:
+            if derived:
+                # Check if Filename exist in the Derives. We sign nothing that not exist.
+                if filename not in skel["derived"]["files"]:
+                    raise errors.NotFound("File not in derives")
+            else:
+                if filename is None:
+                    filename = skel["name"]
+                elif filename != skel["name"]:
+                    raise errors.NotFound("Filename not match")
+
             res = {"downloadUrl": self.create_download_url(dlkey, filename, derived)}
 
         return self.render.view(res)
