ec2c09e29adaedd490a2b7032590c6a91be78a3e
Sven Eberth <se@mausbrand.de>
2024-11-22 16:31:31 +0100

    fix: Revert changes from #1323
    Ensure key is a str for every usage

--- src/viur/core/i18n.py
+++ src/viur/core/i18n.py
@@ -180,8 +180,8 @@ class translate:
         :param public: Flag for public translations, which can be obtained via /json/_translate/get_public.
         """
         super().__init__()
-
-        self.key = str(key).lower()
+        key = str(key)  # ensure key is a str
+        self.key = key.lower()
         self.defaultText = defaultText or key
         self.hint = hint
 
