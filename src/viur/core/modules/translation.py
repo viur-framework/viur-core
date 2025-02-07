831659a5ecaf4a83b58972dfbc5da2669b63a442
Sven Eberth <mail@sveneberth.de>
2025-02-07 20:18:03 +0100

    fix: Make translations usable
    * `tr_key` must be required, otherwise this makes no sense for mappings
    * longer `translations` should be supported too
    * `translations` should not be HTML escaped, sometimes they contains
      links, or words that should be highlighted
    * `tr_key`s are often longer than 50 char, but they should still be
      searchable

--- src/viur/core/modules/translation.py
+++ src/viur/core/modules/translation.py
@@ -9,7 +9,7 @@ from viur.core.decorators import exposed
 from viur.core.bones import *
 from viur.core.i18n import KINDNAME, initializeTranslations, systemTranslations, translate
 from viur.core.prototypes.list import List
-from viur.core.skeleton import Skeleton, SkeletonInstance
+from viur.core.skeleton import Skeleton, SkeletonInstance, ViurTagsSearchAdapter
 
 
 class Creator(enum.Enum):
@@ -20,12 +20,17 @@ class Creator(enum.Enum):
 class TranslationSkel(Skeleton):
     kindName = KINDNAME
 
+    database_adapters = [
+        ViurTagsSearchAdapter(max_length=256),
+    ]
+
     tr_key = StringBone(
         descr=translate(
             "core.translationskel.tr_key.descr",
             "Translation key",
         ),
         searchable=True,
+        required=True,
         unique=UniqueValue(UniqueLockMethod.SameValue, False,
                            "This translation key exist already"),
     )
@@ -37,6 +42,8 @@ class TranslationSkel(Skeleton):
         ),
         searchable=True,
         languages=conf.i18n.available_dialects,
+        escape_html=False,
+        max_length=1024,
         params={
             "tooltip": translate(
                 "core.translationskel.translations.tooltip",
