ba5af19870899871aa6fbcda53a49f014f1e2c6c
Arne Gudermann <ag@mausbrand.de>
2025-04-02 09:19:41 +0200

    Remove old Errors

--- src/viur/core/db/config.py
+++ src/viur/core/db/config.py
@@ -5,22 +5,6 @@ the behaviour of this library.
 conf = {
     # If set, we'll log each query we run
     "traceQueries": False,
-
-    # an allow list which error codes should trigger a verbose message output on stderr/stdout
-    # take a look in :const:`core.db.errors.CANONICAL_ERROR_CODE_MAP` keys and the Exceptions for reference
-    "verbose_error_codes": {
-        "ABORTED",
-        "ALREADY_EXISTS",
-        "DEADLINE_EXCEEDED",
-        "FAILED_PRECONDITION",
-        "INTERNAL",
-        "INVALID_ARGUMENT",
-        "NOT_FOUND",
-        "PERMISSION_DENIED",
-        "RESOURCE_EXHAUSTED",
-        "UNAUTHENTICATED",
-        "UNAVAILABLE"
-    },
     # A Client form the Google Memcache Library.
     "memcache_client": None,
 }
