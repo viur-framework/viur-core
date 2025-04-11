ed58e97202d0f871c2909a69a875b89fb6b6cad8
Sven Eberth <se@mausbrand.de>
2025-04-11 19:13:14 +0200

    fix: Add currentDbAccessLog fallback

--- src/viur/core/db/__init__.py
+++ src/viur/core/db/__init__.py
@@ -1,3 +1,6 @@
+import logging
+import warnings
+
 from . import cache
 from .config import conf as config
 from .query import Query
@@ -6,9 +9,9 @@ from .transport import (AllocateIDs, Count, Delete, Get, Put, RunInTransaction, 
                         run_in_transaction)
 from .types import (DATASTORE_BASE_TYPES, Entity, KEY_SPECIAL_PROPERTY, Key, QueryDefinition, SortOrder,
                     current_db_access_log)
-from .utils import (GetOrInsert, IsInTransaction, is_in_transaction, acquire_transaction_success_marker, encodeKey,
-                    endDataAccessLog, fix_unindexable_properties, keyHelper, normalizeKey, startDataAccessLog,
-                    get_or_insert, normalize_key, key_helper, start_data_access_log, end_data_access_log)
+from .utils import (GetOrInsert, IsInTransaction, acquire_transaction_success_marker, encodeKey, endDataAccessLog,
+                    end_data_access_log, fix_unindexable_properties, get_or_insert, is_in_transaction, keyHelper,
+                    key_helper, normalizeKey, normalize_key, startDataAccessLog, start_data_access_log)
 
 __all__ = [
     "KEY_SPECIAL_PROPERTY",
@@ -48,4 +51,30 @@ __all__ = [
     "key_helper",
     "start_data_access_log",
     "end_data_access_log",
+    "current_db_access_log",
 ]
+
+
+def __getattr__(attr):
+    __DEPRECATED_NAMES = {
+        # stuff prior viur-core < 3.8
+        "currentDbAccessLog": ("current_db_access_log", current_db_access_log),
+    }
+
+    if replace := __DEPRECATED_NAMES.get(attr):
+        msg = f"Use of `utils.{attr}` is deprecated; Use `{replace[0]}` instead!"
+        warnings.warn(msg, DeprecationWarning, stacklevel=3)
+        logging.warning(msg, stacklevel=3)
+
+        ret = replace[1]
+
+        # When this is a string, try to resolve by dynamic import
+        if isinstance(ret, str):
+            mod, item, attr = ret.rsplit(".", 2)
+            mod = __import__(mod, fromlist=(item,))
+            item = getattr(mod, item)
+            ret = getattr(item, attr)
+
+        return ret
+
+    return super(__import__(__name__).__class__).__getattribute__(attr)
