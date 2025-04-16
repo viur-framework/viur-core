20d7d1e376fff4b2cb9f4d316ee7c5375787e6a3
Jan Max Meyer <jmm@phorward.de>
2025-04-16 12:34:09 +0200

    refactor: Deprecate setDefaultLanguage
    Getting rid of yet another ugly, nonsense camel-case function...

--- src/viur/core/__init__.py
+++ src/viur/core/__init__.py
@@ -40,6 +40,7 @@ if not sys.argv[0].endswith("viur-migrate"):  # FIXME: What a "kinda hackish" so
     from viur.core import logging as viurLogging  # unused import, must exist, initializes request logging
 
 import logging  # this import has to stay here, see #571
+from deprecated.sphinx import deprecated
 
 __all__ = [
     # basics from this __init__
@@ -74,12 +75,22 @@ warnings.filterwarnings("once", category=DeprecationWarning)
 warnings.filterwarnings("ignore", category=DeprecationWarning, module=r"viur\.datastore.*",
                         message="'clonedBoneMap' was renamed into 'bone_map'")
 
+
+@deprecated(
+    version="3.8.0",
+    reason="Simply set `conf.i18n.default_language` to the desired language."
+)
 def setDefaultLanguage(lang: str):
     """
         Sets the default language used by ViUR to *lang*.
 
         :param lang: Name of the language module to use by default.
     """
+    import warnings
+    msg = f"`setDefaultLanguage(\"{lang}\")` is deprecated; Replace the call by `conf.i18n.default_language = \"{lang.lower()}\"`"
+    warnings.warn(msg, DeprecationWarning, stacklevel=2)
+    logging.warning(msg)
+
     conf.i18n.default_language = lang.lower()
 
 
