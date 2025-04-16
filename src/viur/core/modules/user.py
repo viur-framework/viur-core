79205c4d35d9334a9ac8520e4e62fdefef6c81b6
Arne Gudermann <ag@mausbrand.de>
2025-04-16 09:35:43 +0200

    PEP 8

--- src/viur/core/modules/user.py
+++ src/viur/core/modules/user.py
@@ -1579,8 +1579,8 @@ class User(List):
         # In case the user is set to inactive, kill all sessions
         if self.is_active(skel) is False:
             session.killSessionByUser(skel["key"])
-        #Update all Sessions
-        for session_obj in db.Query("user").filter("user =",skel["key"]).iter():
+        # Update all Sessions
+        for session_obj in db.Query("user").filter("user =", skel["key"]).iter():
             session_obj["data"]["user"] = skel.dbEntity
 
 
