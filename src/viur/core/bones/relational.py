4f5b6e6b920a27dd8974c87404d91f8458b8fa68
Stefan Kögl <sk@mausbrand.de>
2025-09-16 13:47:47 +0200

    chore: add bone name in assert error for better debugging in RelationalBone

--- src/viur/core/bones/relational.py
+++ src/viur/core/bones/relational.py
@@ -385,7 +385,7 @@ class RelationalBone(BaseBone):
             return None
         elif isinstance(value, list) and value:
             value = value[0]
-        assert isinstance(value, dict), f"Read something from the datastore thats not a dict: {type(value)}"
+        assert isinstance(value, dict), f"Read something from the datastore that's not a dict: {self.name=} -> {type(value)}"
         if "dest" not in value:
             return None
         relSkel, usingSkel = self._getSkels()
