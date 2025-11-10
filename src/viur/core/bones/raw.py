72ed944d1d44e05f92f9dfbb3e35cd6467481492
Jan Max Meyer <jmm@phorward.de>
2025-11-10 22:55:55 +0100

    fix: `RawBone` looses its value when invalid
    When something entered is wrong, it should not be returned the empty value.

--- src/viur/core/bones/raw.py
+++ src/viur/core/bones/raw.py
@@ -6,9 +6,10 @@ SEARCH_TAGS = re.compile(r"[^\s]+")
 
 class RawBone(BaseBone):
     """
-    Stores its data without applying any pre/post-processing or filtering. Can be used to store
-    non-html content.
-    Use the dot-notation like "raw.markdown" or similar to describe subsequent types.
+    Stores its data without applying any pre/post-processing or filtering.
+    Can be used to store any textual content.
+
+    Use the dot-notation like "raw.code.markdown" or similar to describe subsequent types.
 
     ..Warning: Using this bone will lead to security vulnerabilities like reflected XSS unless the
         data is either otherwise validated/stripped or from a trusted source! Don't use this unless
@@ -17,9 +18,9 @@ class RawBone(BaseBone):
     type = "raw"
 
     def singleValueFromClient(self, value, skel, bone_name, client_data):
-        err = self.isInvalid(value)
-        if err:
-            return self.getEmptyValue(), [ReadFromClientError(ReadFromClientErrorSeverity.Invalid, err)]
+        if err := self.isInvalid(value):
+            return value, [ReadFromClientError(ReadFromClientErrorSeverity.Invalid, err)]
+
         return value, None
 
     def getSearchTags(self, skel: "SkeletonInstance", name: str) -> set[str]:
