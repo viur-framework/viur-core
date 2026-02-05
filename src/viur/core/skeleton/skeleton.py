612600790658d089f44f5772d3ab86c91629aaf6
Sven Eberth <mail@sveneberth.de>
2026-02-05 02:35:55 +0100

    chore: clean up

--- src/viur/core/skeleton/skeleton.py
+++ src/viur/core/skeleton/skeleton.py
@@ -95,7 +95,7 @@ class Skeleton(BaseSkeleton, metaclass=MetaSkel):
 
     shortkey = RawBone(
         descr="Shortkey",
-        compute=Compute(lambda skel: skel.dbEntity.key.id_or_name if skel.dbEntity and skel.dbEntity.key else None),
+        compute=Compute(lambda skel: skel["key"].id_or_name if skel["key"] else None),
         readOnly=True,
         visible=False,
         searchable=True,
