60d5d1059e8f971b85499b4bbaa4ec6d2d203dc2
Sven Eberth <mail@sveneberth.de>
2026-02-24 14:43:21 +0100

    refactor: Use `KeyType` for `ìsinstance` checks

--- src/viur/core/bones/relational.py
+++ src/viur/core/bones/relational.py
@@ -10,7 +10,7 @@ import typing as t
 import warnings
 from itertools import chain
 
-from viur.core import db, utils, i18n
+from viur.core import db, i18n, utils
 from viur.core.bones.base import BaseBone, ReadFromClientError, ReadFromClientErrorSeverity, getSystemInitialized
 
 if t.TYPE_CHECKING:
@@ -428,7 +428,6 @@ class RelationalBone(BaseBone):
                 using_data_serialized = None
 
             return using_data_serialized, ref_data_serialized
-
 
         super().serialize(skel, name, parentIndexed)
 
@@ -1168,11 +1167,11 @@ class RelationalBone(BaseBone):
             Furthermore, the second field must be a skeletonInstanceClassRef.
             """
             return (isinstance(in_value, tuple) and len(in_value) == 2
-                        and isinstance(in_value[0], (str, int, db.Key))
-                        and isinstance(in_value[1], self._skeletonInstanceClassRef))
+                    and isinstance(in_value[0], KeyType)
+                    and isinstance(in_value[1], self._skeletonInstanceClassRef))
 
         if not self.multiple and not self.using:
-            if not isinstance(value, (str, int, db.Key)):
+            if not isinstance(value, KeyType):
                 raise ValueError(f"You must supply exactly one Database-Key str or int to {boneName}")
             parsed_value = (value, None)
         elif not self.multiple and self.using:
@@ -1180,8 +1179,11 @@ class RelationalBone(BaseBone):
                 raise ValueError(f"You must supply a tuple of (Database-Key, relSkel) to {boneName}")
             parsed_value = value
         elif self.multiple and not self.using:
-            if not isinstance(value, (str, int, db.Key)) and not (isinstance(value, list)) \
-                    and all([isinstance(val, (str, int, db.Key)) for val in value]):
+            if (
+                not isinstance(value, KeyType)
+                and not (isinstance(value, list))
+                and all(isinstance(val, KeyType) for val in value)
+            ):
                 raise ValueError(f"You must supply a Database-Key or a list hereof to {boneName}")
             if isinstance(value, list):
                 parsed_value = [(key, None) for key in value]
